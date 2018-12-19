80aeb0e2c216da103f615c4beb334b1cc3a9a097
yangguanbao <yangguanbao@qq.com>
2018-12-19 16:56:30 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Goods.php
+++ thinkphp5.1/weiya/application/index/controller/Goods.php
@@ -116,6 +116,16 @@ class Goods extends \common\controller\Base{
             $info['detail_img'] = explode(',',(string)$info['detail_img']);
             $info['tag'] = explode(',',(string)$info['tag']);
             $this->assign('info',$info);
+            
+            $where = [
+                ['status','=',0],
+                ['goods_id','=',$id],
+            ];
+            $averageScore = $model -> where($where)->avg('score');
+            $this ->assign('averageScore',$averageScore);
+            $total = $model -> where($where)->count('user_id');
+            $this ->assign('total',$total);
+
             $unlockingFooterCart = unlockingFooterCartConfig([0,2,1]);
             $this->assign('unlockingFooterCart', $unlockingFooterCart);
             return $this->fetch();
