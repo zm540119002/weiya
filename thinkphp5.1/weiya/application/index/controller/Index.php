e3b05a8232b9e879c8a4811b2bb433444391c401
zhangmin <540119002@qq.com>
2018-12-19 11:10:46 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -5,6 +5,14 @@ class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
+        $conn=mysql_connect('127.0.0.1','root','msy');
+        if ($conn){
+            echo "LNMP platform connect to mysql is successful!";
+        }else{
+            echo "LNMP platform connect to mysql is failed!";
+        }
+        phpinfo();
+        exit;
         //获取商品的分类
         $modelGoodsCategory = new \app\index\model\GoodsCategory();
         $config =[
