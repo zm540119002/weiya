1593e9f5cae98b0fb99794f42cb18a2c06fc4951
yangguanbao <yangguanbao@qq.com>
2018-12-20 11:06:23 +0800

    ygb

--- thinkphp5.1/weiya/application/index_admin/controller/Goods.php
+++ thinkphp5.1/weiya/application/index_admin/controller/Goods.php
@@ -492,7 +492,7 @@ class Goods extends Base {
                 ['rg.goods_id', '=', $goodsId],
             ],'field'=>[
                 'g.id ','g.headline','g.thumb_img','g.bulk_price','g.specification','g.minimum_order_quantity',
-                'g.minimum_sample_quantity','g.increase_quantity','g.purchase_unit'
+                'g.minimum_sample_quantity','g.increase_quantity','g.purchase_unit','g.name'
             ],'join'=>[
                 ['goods g','g.id = rg.recommend_goods_id','left']
             ]
