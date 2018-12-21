af68d74080c2202a8e5dab1290ebd54e13415f39
yangguanbao <yangguanbao@qq.com>
2018-12-21 15:33:15 +0800

    ygb

--- thinkphp5.1/weiya/application/index/config/footer_menu.php
+++ thinkphp5.1/weiya/application/index/config/footer_menu.php
@@ -18,14 +18,15 @@ return [
     'menu' => [
         ['name'   => '总金额','class'=>'amount'],//0
         ['name'   => '加入购物车','class'=>'add_cart'],//1
-        ['name'   => '购物车','class'=>'add_cart_icon'],//1
-        ['name'   => '立即购买','class'=>'buy_now'],//2
-        ['name'   => '提交订单','class'=>'confirm_order'],//3
-        ['name'   => '支付','class'=>'pay'],//4
-        ['name'   => '增加地址','class'=>'address_save'],//5
-        ['name'   => '修改地址','class'=>'address_save'],//6
-        ['name'   => '新建地址','class'=>'address_create'],//7
-        
+        ['name'   => '购物车','class'=>'add_cart_icon'],//2
+        ['name'   => '立即购买','class'=>'buy_now'],//3
+        ['name'   => '提交订单','class'=>'confirm_order'],//4
+        ['name'   => '支付','class'=>'pay'],//5
+        ['name'   => '增加地址','class'=>'address_save'],//6
+        ['name'   => '修改地址','class'=>'address_save'],//7
+        ['name'   => '新建地址','class'=>'address_create'],//8
+        ['name'   => 'ddd','class'=>'nc'],//9
+
         
     ],
 
