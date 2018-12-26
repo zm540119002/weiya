11f7d102f3b08516f71ad81cdd044567380ad437
yangguanbao <yangguanbao@qq.com>
2018-12-26 11:03:06 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Cart.php
+++ thinkphp5.1/weiya/application/index/controller/Cart.php
@@ -68,6 +68,7 @@ class Cart extends \common\controller\Base{
              $config['where'][] = ['g.name', 'like', '%' . trim($keyword) . '%'];
          }
          $list = $model -> pageQuery($config)->toArray();
+        print_r($list);exit;
          $this->assign('list',$list);
          if(isset($_GET['pageType'])){
              if($_GET['pageType'] == 'index' ){//店铺产品列表
