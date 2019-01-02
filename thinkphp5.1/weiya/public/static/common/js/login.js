9536b5cd398d19823c84b3bc55755203cdcbf095
zhangmin <540119002@qq.com>
2019-01-02 16:13:54 +0800

    zm

--- thinkphp5.1/weiya/public/static/common/js/login.js
+++ thinkphp5.1/weiya/public/static/common/js/login.js
@@ -92,6 +92,9 @@ $(function(){
                     if(loginSign=='page'){
                         location.href = data.info;
                     }else if(loginSign=='dialog'){
+                        console.log(typeof dialogLoginCallBack);
+                        console.log(dialogLoginCallBack);
+                        return;
                         if(typeof(dialogLoginCallBack) == "undefined"){
                             dialogLoginDefaultCallBack(data);
                         }else if($.isFunction(dialogLoginCallBack)){
