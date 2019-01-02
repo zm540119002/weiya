4828e160538970e3ea32f59000432f9cf549a176
zhangmin <540119002@qq.com>
2019-01-02 09:55:16 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -5,9 +5,6 @@ class Index extends \common\controller\UserBase{
     /**首页
      */
     public function index(){
-        print_r(123);
-
-        exit;
         //获取商品的分类
         $modelGoodsCategory = new \app\index\model\GoodsCategory();
         $config =[
