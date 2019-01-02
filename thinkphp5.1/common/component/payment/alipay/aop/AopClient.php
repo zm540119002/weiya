13d3ade733fbf46f831cd45196368cf18f0ea44d
yangguanbao <yangguanbao@qq.com>
2019-01-02 09:54:45 +0800

    ygb

--- thinkphp5.1/common/component/payment/alipay/aop/AopClient.php
+++ thinkphp5.1/common/component/payment/alipay/aop/AopClient.php
@@ -578,8 +578,8 @@ class AopClient {
 		$sign = $params['sign'];
 		$params['sign_type'] = null;
 		$params['sign'] = null;
-		print_r($params);exit;
-		return $this->verify($this->getSignContent($params), $sign, $rsaPublicKeyFilePath,$signType);
+		$a = $this->verify($this->getSignContent($params), $sign, $rsaPublicKeyFilePath,$signType);
+		print_r($a);exit;
 	}
 	public function rsaCheckV2($params, $rsaPublicKeyFilePath, $signType='RSA') {
 		$sign = $params['sign'];
