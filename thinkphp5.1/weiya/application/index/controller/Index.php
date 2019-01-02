63b5817d30135633c9bc06e483374f4af99e1bf0
zhangmin <540119002@qq.com>
2019-01-02 10:30:45 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\controller;
 
-class Index extends \common\controller\UserBase{
+class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
