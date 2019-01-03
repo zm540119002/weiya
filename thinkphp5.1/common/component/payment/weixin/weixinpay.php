9f316f3e460ff175ed6b9a8b4331649528499069
yangguanbao <yangguanbao@qq.com>
2019-01-03 12:14:20 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/weixinpay.php
+++ thinkphp5.1/common/component/payment/weixin/weixinpay.php
@@ -34,7 +34,6 @@ class weixinpay{
      * @param  string   $total_fee  金额
      */
     public static function getJSAPI($payInfo){
-        print_r($payInfo['sn']);exit;
         $input = new \WxPayUnifiedOrder();
         $tools = new \JsApiPay();
         $openId = $tools->GetOpenid();
