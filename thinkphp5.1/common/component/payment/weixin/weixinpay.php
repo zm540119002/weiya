c4a9f5d026c80b8bea3761afb0550db15da209ea
yangguanbao <yangguanbao@qq.com>
2019-01-03 13:50:45 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/weixinpay.php
+++ thinkphp5.1/common/component/payment/weixin/weixinpay.php
@@ -35,13 +35,12 @@ class weixinpay{
      */
     public static function getJSAPI($payInfo){
         $payInfo['return_url'] = $payInfo['return_url']?:url('Index/index');
-        $trade_no = $payInfo['sn'];
         $tools = new \JsApiPay();
         $openId = $tools->GetOpenid();
         $input = new \WxPayUnifiedOrder();
         $input->SetBody('美尚云');					//商品名称
         $input->SetAttach($payInfo['attach']);					//附加参数,可填可不填,填写的话,里边字符串不能出现空格
-        $input->SetOut_trade_no($trade_no);			//订单号
+        $input->SetOut_trade_no( $payInfo['sn'] );			//订单号
         $input->SetTotal_fee($payInfo['actually_amount'] * 100);			//支付金额,单位:分
         $input->SetTime_start(date("YmdHis"));		//支付发起时间
         $input->SetTime_expire(date("YmdHis", time() + 600));//支付超时
