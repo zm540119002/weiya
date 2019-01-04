45f4b3f489831c722ba85ad582e4b2c79516a413
yangguanbao <yangguanbao@qq.com>
2019-01-04 15:40:54 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/CallBack.php
+++ thinkphp5.1/weiya/application/index/controller/CallBack.php
@@ -271,25 +271,25 @@ class CallBack extends \common\controller\Base
             return errorMsg($modelOrder->getLastSql());
         }
         
-        //根据订单号查询关联的商品
-        $modelOrderDetail = new \app\index\model\OrderDetail();
-        $config = [
-            'where' => [
-                ['od.status', '=', 0],
-                ['od.father_order_id', '=', $orderInfo['id']],
-            ], 'field' => [
-                'od.goods_id', 'od.price', 'od.num', 'od.store_id',
-            ]
-        ];
-        $orderDetailList = $modelOrderDetail->getList($config);
-        $modelOrderChild = new \app\index\model\OrderChild();
-
-        //生成子订单
-        $rse = $modelOrderChild -> createOrderChild($orderDetailList);
-        if(!$rse['status']){
-            $modelOrder->rollback();
-            return errorMsg($modelOrder->getLastSql());
-        }
+//        //根据订单号查询关联的商品
+//        $modelOrderDetail = new \app\index\model\OrderDetail();
+//        $config = [
+//            'where' => [
+//                ['od.status', '=', 0],
+//                ['od.father_order_id', '=', $orderInfo['id']],
+//            ], 'field' => [
+//                'od.goods_id', 'od.price', 'od.num', 'od.store_id',
+//            ]
+//        ];
+//        $orderDetailList = $modelOrderDetail->getList($config);
+//        $modelOrderChild = new \app\index\model\OrderChild();
+//
+//        //生成子订单
+//        $rse = $modelOrderChild -> createOrderChild($orderDetailList);
+//        if(!$rse['status']){
+//            $modelOrder->rollback();
+//            return errorMsg($modelOrder->getLastSql());
+//        }
         $modelOrder->commit();//提交事务
         //返回状态给微信服务器
         return successMsg('成功');
