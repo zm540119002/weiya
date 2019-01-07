0f97fffd39f848a9160778ac57e00d71ad0b124c
yangguanbao <yangguanbao@qq.com>
2019-01-07 16:29:01 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/CallBack.php
+++ thinkphp5.1/weiya/application/index/controller/CallBack.php
@@ -8,31 +8,6 @@ class CallBack extends \common\controller\Base
 {
     public function weixinBack(){
         $xml = file_get_contents('php://input');
-        $xml ="
-        <xml>
-   <appid><![CDATA[wx2421b1c4370ec43b]]></appid>
-   <attach><![CDATA[支付测试]]></attach>
-   <bank_type><![CDATA[CFT]]></bank_type>
-   <fee_type><![CDATA[CNY]]></fee_type>
-   <is_subscribe><![CDATA[Y]]></is_subscribe>
-   <mch_id><![CDATA[10000100]]></mch_id>
-   <nonce_str><![CDATA[5d2b6c2a8db53831f7eda20af46e531c]]></nonce_str>
-   <openid><![CDATA[oUpF8uMEb4qRXf22hE3X68TekukE]]></openid>
-   <out_trade_no><![CDATA[20190107162508370676664342445210]]></out_trade_no>
-   <result_code><![CDATA[SUCCESS]]></result_code>
-   <return_code><![CDATA[SUCCESS]]></return_code>
-   <sign><![CDATA[B552ED6B279343CB493C5DD0D78AB241]]></sign>
-   <sub_mch_id><![CDATA[10000100]]></sub_mch_id>
-   <time_end><![CDATA[20140903131540]]></time_end>
-   <total_fee>1</total_fee>
- <coupon_fee_0><![CDATA[10]]></coupon_fee_0>
- <coupon_count><![CDATA[1]]></coupon_count>
- <coupon_type><![CDATA[CASH]]></coupon_type>
- <coupon_id><![CDATA[10000]]></coupon_id> 
-   <trade_type><![CDATA[JSAPI]]></trade_type>
-   <transaction_id><![CDATA[1004400740201409030005092168]]></transaction_id>
- </xml> 
-        ";
         $data = xmlToArray($xml);
         $data_sign = $data['sign'];
         //sign不参与签名算法
@@ -44,11 +19,11 @@ class CallBack extends \common\controller\Base
         $data['order_sn'] = $data['out_trade_no'];//系统的订单号
         $data['payment_time'] = $data['time_end'];//支付时间
 
-//        // 判断签名是否正确  判断支付状态
-//        if ( ($data_sign !== $sign) || ($data['return_code'] !== 'SUCCESS') || ($data['result_code'] !== 'SUCCESS')) {
-//            //返回状态给微信服务器
-//            $this->errorReturn($data['out_trade_no']);
-//        }
+        // 判断签名是否正确  判断支付状态
+        if ( ($data_sign !== $sign) || ($data['return_code'] !== 'SUCCESS') || ($data['result_code'] !== 'SUCCESS')) {
+            //返回状态给微信服务器
+            $this->errorReturn($data['out_trade_no']);
+        }
 
         $order_type = '';
         if(input('?type')){
