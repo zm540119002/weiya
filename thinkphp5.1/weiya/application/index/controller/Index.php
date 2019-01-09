93b23d2b6f25ff16d0edb4355bfd4542690821de
yangguanbao <yangguanbao@qq.com>
2019-01-09 16:42:35 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -6,7 +6,7 @@ class Index extends \common\controller\Base{
      */
     public function index(){
         $a = request()->domain();
-        print_r($a);exit;
+        print_r(1111);exit;
         //获取商品的分类
         $modelGoodsCategory = new \app\index\model\GoodsCategory();
         $config =[
