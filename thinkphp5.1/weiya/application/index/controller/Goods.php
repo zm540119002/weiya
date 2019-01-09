53be3bd584ae320fa2ae75b084ecb44aaf9d6e0f
yangguanbao <yangguanbao@qq.com>
2019-01-09 10:00:11 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Goods.php
+++ thinkphp5.1/weiya/application/index/controller/Goods.php
@@ -83,7 +83,6 @@ class Goods extends \common\controller\Base{
         }
         
         $list = $model -> pageQuery($config);
-        print_r($model->getLastSql());exit;
         $this->assign('list',$list);
         if(isset($_GET['pageType'])){
             if($_GET['pageType'] == 'index' ){
