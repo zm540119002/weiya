615fcb9269237031124c7b4224d4499a2fdabefb
yangguanbao <yangguanbao@qq.com>
2019-01-10 09:04:14 +0800

    ygb

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -8,7 +8,6 @@ class Base extends \think\Controller{
     protected $host;
     public function __construct(){
         parent::__construct();
-        header("Content-Security-Policy: upgrade-insecure-requests");
         //登录验证后跳转回原验证发起页
         $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
                 && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
