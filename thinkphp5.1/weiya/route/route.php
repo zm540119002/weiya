36a4c952fd9812531da17acba5bc17350c250754
zhangmin <540119002@qq.com>
2019-01-10 15:03:16 +0800

    zm

--- thinkphp5.1/weiya/route/route.php
+++ thinkphp5.1/weiya/route/route.php
@@ -12,9 +12,7 @@
 Route::get('think', function () {
     return 'hello,ThinkPHP555555555555555555555555!';
 });
-
-// 完整域名绑定到index模块
-//Route::domain('www.worldview.com.cn', 'index/Index/index');
+Route::get('/','index/Index/index');
 return [
 
 ];
