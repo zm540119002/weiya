f056fbe1363fff0e587b77892c089ab9727de9cf
yangguanbao <yangguanbao@qq.com>
2019-01-11 17:53:42 +0800

    ygb

--- thinkphp5.1/weiya/public/static/common/js/wallet.js
+++ thinkphp5.1/weiya/public/static/common/js/wallet.js
@@ -1,5 +1,5 @@
 //钱包支付弹窗
-function walletPayDialog() {
+function walletPayDialog(fn_name) {
     var content=$('#walletPay').html();
     window.scrollTo(0,0);
     layer.open({
@@ -14,7 +14,6 @@ function walletPayDialog() {
             var oLis=$('.payPasswordLayer input.password_item');
             for(var i = 0;i<oLis.length;i++){
                 var obj=oLis[i];
-                console.log(obj);
                 $(obj).data('index',i);
                 $(obj).attr('readonly',true);
                 $(obj).on('keyup',function(){
@@ -22,7 +21,6 @@ function walletPayDialog() {
                     _this.val().replace(/^(.).*$/,'$1');
                     _this.attr('readonly',true);
                     var next=_this.data('index')+1;
-                    console.log(next);
                     if(next>oLis.length-1){
                         console.log('12345678');
                     }
@@ -34,17 +32,29 @@ function walletPayDialog() {
         },
         yes:function(index){
             var oLis=$('.payPasswordLayer input.password_item');
-            var postData='';
+            var password='';
             for(var i=0;i<oLis.length;i++){
-                postData=postData+$(oLis[i]).val();
+                password=password+$(oLis[i]).val();
             }
-            console.log(postData);
-            if(postData.length<4){
+            if(password.length<4){
                 dialog.error('请输入正确4位数密码');
                 return false;
             }
+            var postData ={
+                password:password,
+            }
+            var url = module+'Wallet/login';
             $.post(url,postData,function (data) {
-                layer.close(index);
+                if(data.status){
+                    if(data.fn_name){
+                        var str = data.fn_name;
+                        eval(str +"()");
+                        $('.layui-m-layer').remove();
+                        cancleFixedLayer();
+                        return false;
+                    }
+                }
+                // layer.close(index);
             })
             
         }
@@ -68,208 +78,40 @@ function forgetWalletPasswordDialog(){
             var postForm = $('.forgetWalletPasswordLayer #ForgetWalletPassword');
             var content='';
             var postData = postForm.serializeObject();
-            console.log(postData);
-            if(!register.vfyCheck(postData.captcha)){
-                content = "请输入正确的验证码";
-            }else if(!register.pswCheck(postData.password)){
-                content = "请输入6-16数字或字母的密码";
-            }
+            // if(!register.vfyCheck(postData.captcha)){
+            //     content = "请输入正确的验证码";
+            // }else if(!register.pswCheck(postData.password)){
+            //     content = "请输入4数字或字母的密码";
+            // }
             if(content){
                 dialog.error(content);
                 return false;
             }
+            var url = module+'Wallet/forgetPassword';
             $.post(url,postData,function (data) {
 
             })
         }
     });
 }
-
-
-$(function(){
-    //忘记密码-弹窗事件
-    $('body').on('click','#login_dialog',function(){
-        loginDialog('reload');
-    });
-    //忘记密码-弹窗事件
-    $('body').on('click','#logout_dialog',function(){
-        logoutDialog();
-    });
-    //忘记密码-弹窗事件
-    $('body').on('click','.forget_wallet_password',function(){
-        //var fn_name = $(this).siblings('input[name="fn_name"]').val();
-        forgetWalletPasswordDialog();
-    });
-    //钱包支付密码
-     function walletPayPassword(oLis){
-       
-     }
-});
-$(function(){
-    //登录 / 注册-切换
-    tab_down('.loginNav li','.loginTab ','click');
-    //登录 or 注册 or 重置密码
-    $('body').on('click','.loginBtn,.registerBtn,.comfirmBtn',function(){
-        var _this = $(this);
-        var method = _this.data('method');
-        var content='';
-        var url = domain+'ucenter/UserCenter/'+method;
-        var postForm = null;
-        var loginSign = 'dialog';
-        if(method=='login'){//登录
-            if($('.loginLayer #formLogin').length){//弹框登录
-                postForm = $('.loginLayer #formLogin');
-            }else{//页面登录
-                loginSign = 'page';
-                postForm = $('#formLogin');
-            }
-        }else if(method=='register'){//注册
-            if($('.loginLayer #formRegister').length){//弹框注册
-                postForm = $('.loginLayer #formRegister');
-            }else{//页面注册
-                loginSign = 'page';
-                postForm = $('#formRegister');
-            }
-        }else if(method=='forgetPassword'){//重置密码
-            console.log($('.forgetPasswordLayer  #formForgetPassword').length);
-            if($('.forgetPasswordLayer  #formForgetPassword').length){//弹框重置密码
-                postForm = $('.forgetPasswordLayer #formForgetPassword');
-            }else{//页面重置密码
-                loginSign = 'page';
-                postForm = $('#formForgetPassword');
-            }
-        }
-        if(!postForm){
-            dialog.error('未知操作');
-            return false;
-        }
-        var postData = postForm.serializeObject();
-        if(!register.phoneCheck(postData.mobile_phone)){
-            content='请输入正确手机号码';
-        }else if(method!='login' && !register.vfyCheck(postData.captcha)){
-            content = "请输入正确的验证码";
-        }else if(!register.pswCheck(postData.password)){
-            content = "请输入6-16数字或字母的密码";
-        }
-        if(method && content){
-            dialog.error(content);
-            return false;
-        }else if(content){
-            errorTipc(content);
-            return false;
-        }else{
-            $.post(url,postData,function (data) {
-                if(data.status==0){
-                    dialog.error(data.info);
-                    return false;
-                }else if(data.status==1){
-                    if(loginSign=='page'){
-                        location.href = data.info;
-                    }else if(loginSign=='dialog'){
-                        if(data.fn_name){
-                            var str = data.fn_name;
-                            eval(str +"()");
-                            $('.layui-m-layer').remove();
-                            cancleFixedLayer();
-                            return false;
-                        }
-                        dialogLoginDefaultCallBack(data);
-                        // if(typeof(dialogLoginCallBack) == "undefined" || !$.isFunction(dialogLoginCallBack)){
-                        //     dialogLoginDefaultCallBack(data)
-                        // }else{
-                        //     dialogLoginCallBack(data);
-                        // }
-                        
-                    }
-                }
-            });
+//订单支付
+function orderPay(postData) {
+    var url = module + 'Payment/orderPayment';
+    $.ajax({
+        url: url,
+        data: postData,
+        type: 'post',
+        beforeSend: function(){
+            $('.loading').show();
+        },
+        error:function(){
+            $('.loading').hide();
+            dialog.error('AJAX错误');
+        },
+        success: function(data){
+            obj.removeClass("nodisabled");//防止重复提交
+            $('.loading').hide();
+            location.href = module + 'Order/confirmOrder/order_sn/' + data.order_sn;
         }
     });
-    //弹框登录成功默认回调函数
-    function dialogLoginDefaultCallBack(data) {
-        location.href = data.info;
-    }
-    function reload() {
-        location.reload();
-    }
-    //显示隐藏密码
-    //var onOff = true;
-    $('body').on('click','.view-password',function(){
-        var _this=$(this);
-        //_this.toggleClass('active');
-        if(_this.prev().attr('type')=='password'){
-            $('.login_item .password').attr('type','text');
-            $('.view-password').addClass('active');
-        }else{
-            $('.login_item .password').attr('type','password');
-            $('.view-password').removeClass('active');
-        }
-    });
-    //获取验证码
-    var timer;
-    var requestSign = true;
-    $('body').on('click','.send_sms',function(){
-        if($(this).attr('disabled')){
-            return false;
-        }
-        var _form = $(this).parents('form');
-        var postData = {};
-        postData.mobile_phone = _form.find('[name=mobile_phone]').val();
-        var userPhone=_form.find('.user_phone').val();
-        if(!requestSign){
-            return false;
-        }
-        var time=60;
-        var content='';
-        if(!register.phoneCheck(userPhone)){
-            content='请输入正确手机号码';
-        }
-        if(content){
-            errorTipc(content);
-            return false;
-        }
-        $('.tel_code').val('');
-        clearInterval(timer);
-        timer=setInterval(CountDown,1000);
-        function CountDown(){
-            _form.find('.send_sms').attr('disabled',true);
-            _form.find('.send_sms').text('重新发送'+time+'s');
-            if(time==0){
-                _form.find('.send_sms').text("获取验证码").removeAttr("disabled");
-                _form.find('.tel_code').val('');
-                clearInterval(timer);
-            }
-            time--;
-        }
-        var url = send_sms;
-        $.post(url,postData,function(msg){
-            requestSign = true;
-            if(msg.status == 0){
-                $('.phone').val('').removeAttr("disabled");
-                _form.find('.send_sms').val("获取验证码").removeAttr("disabled");
-                _form.find('.tel_code').val('');
-                clearInterval(timer);
-                errorTipc(msg.info,3000);
-                return false;
-            }else if(msg.status == 1){
-                errorTipc("验证码已发送至手机:"+ postData.mobile_phone +' ，请查看。',3000);
-                return false;
-            }
-        });
-    });
-    //使用须知
-    var attentionForm=$('#attentionForm').html();
-    $('body').on('click','.use-attention',function(){
-        var pageii = layer.open({
-            title:['《美尚平台使用须知》','border-bottom:1px solid #d9d9d9;'],
-            className:'addCcountLayer',
-            type: 1,
-            content: attentionForm,
-            anim: 'up',
-            style: 'position:fixed; left:0; top:0; width:100%; height:100%; border: none; -webkit-animation-duration: .5s; animation-duration: .5s;',
-            success:function(){
-            },
-            btn:['确定']
-        });
-    });
-});
+}
