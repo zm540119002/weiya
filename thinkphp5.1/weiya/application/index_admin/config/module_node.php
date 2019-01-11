8ae2b9be91349b6692953a5199c86b13c7a222a1
zhangmin <540119002@qq.com>
2019-01-11 16:15:22 +0800

    zm

--- thinkphp5.1/weiya/application/index_admin/config/module_node.php
+++ thinkphp5.1/weiya/application/index_admin/config/module_node.php
@@ -3,34 +3,29 @@
  * display    1：显示|0：隐藏
  */
 return [
-    'menu' => [
-        'goods'=>[
-            'id'=>100,'name'=>'商品','type'=>2,
-            'sub_menu' => [
-                ['id'=>101,'name'=>'商品分类管理','display'=>1,'controller'=>'GoodsCategory','action'=>'manage',],
-                ['id'=>102,'name'=>'商品管理','display'=>1,'controller'=>'Goods','action'=>'manage',],
-            ],
+    'goods'=>[
+        'id'=>100,'name'=>'商品','type'=>2,
+        'sub_menu' => [
+            ['id'=>101,'name'=>'商品分类管理','display'=>1,'controller'=>'GoodsCategory','action'=>'manage',],
+            ['id'=>102,'name'=>'商品管理','display'=>1,'controller'=>'Goods','action'=>'manage',],
         ],
-        'scene'=>[
-            'id'=>200,'name'=>'场景','type'=>2,
-            'sub_menu' => [
-//                ['id'=>201,'name'=>'项目分类管理','display'=>1,'controller'=>'ProjectCategory','action'=>'manage',],
-                ['id'=>202,'name'=>'场景管理','display'=>1,'controller'=>'Scene','action'=>'manage',],
-            ],
+    ],
+    'scene'=>[
+        'id'=>200,'name'=>'场景','type'=>2,
+        'sub_menu' => [
+            ['id'=>202,'name'=>'场景管理','display'=>1,'controller'=>'Scene','action'=>'manage',],
         ],
-        'project'=>[
-            'id'=>300,'name'=>'项目','type'=>2,
-            'sub_menu' => [
-//                ['id'=>201,'name'=>'项目分类管理','display'=>1,'controller'=>'ProjectCategory','action'=>'manage',],
-                ['id'=>301,'name'=>'项目管理','display'=>1,'controller'=>'Project','action'=>'manage',],
-            ],
+    ],
+    'project'=>[
+        'id'=>300,'name'=>'项目','type'=>2,
+        'sub_menu' => [
+            ['id'=>301,'name'=>'项目管理','display'=>1,'controller'=>'Project','action'=>'manage',],
         ],
-        'information'=>[
-            'id'=>300,'name'=>'资讯','type'=>2,
-            'sub_menu' => [
-//                ['id'=>201,'name'=>'项目分类管理','display'=>1,'controller'=>'ProjectCategory','action'=>'manage',],
-                ['id'=>301,'name'=>'资讯管理','display'=>1,'controller'=>'Information','action'=>'manage',],
-            ],
+    ],
+    'information'=>[
+        'id'=>400,'name'=>'资讯','type'=>2,
+        'sub_menu' => [
+            ['id'=>401,'name'=>'资讯管理','display'=>1,'controller'=>'Information','action'=>'manage',],
         ],
     ],
 ];
