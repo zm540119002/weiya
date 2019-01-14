a1a5efa14f1ba3e179727dc0924e99be6ed41940
zhangmin <540119002@qq.com>
2019-01-14 14:43:44 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -1,12 +1,9 @@
 <?php
 namespace app\index\controller;
-use think\facade\Request;
 class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
-        print_r(config('template.tpl_replace_string'));
-        exit;
         //获取商品的分类
         $modelGoodsCategory = new \app\index\model\GoodsCategory();
         $config =[
