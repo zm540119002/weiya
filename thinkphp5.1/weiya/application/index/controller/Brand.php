5df1c70415b0aed87e3a4a47485662f7ef3a0f99
yangguanbao <yangguanbao@qq.com>
2019-01-16 15:56:38 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Brand.php
+++ thinkphp5.1/weiya/application/index/controller/Brand.php
@@ -5,12 +5,21 @@ class Brand extends \common\controller\Base{
     /**首页
      */
     public function index(){
+        $brandInfo = [
+            'brand_type'=>1,
+            'brand_name'=>'gaasg',
+            'brand_logo'=>'temp/2019011615535029895.jpeg',
+            'trademark_certificate'=>'temp/2019011615535288073.jpeg',
+            'trademark_authorization'=>'temp/2019011615535288073.jpeg',
+        ];
+        $this->assign('info',$brandInfo);
         $unlockingFooterCart = unlockingFooterCartConfig([15]);
         $this->assign('unlockingFooterCart', $unlockingFooterCart);            
         return $this->fetch();
     }
 
     public function edit(){
+
         return $this->fetch('info_tpl');
     }
 }
