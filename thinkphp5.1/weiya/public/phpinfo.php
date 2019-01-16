8391b0b91f80829ecd252b34e5eddb2b7bc68e8a
zm <zm@example.com>
2019-01-16 09:57:01 +0800

    zm

--- thinkphp5.1/weiya/public/phpinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysql_connect('127.0.0.1','root','msy');
-if ($conn){
- echo "LNMP platform connect to mysql is successful!";
-}else{
- echo "LNMP platform connect to mysql is failed!";
-}
-phpinfo();
-?>
