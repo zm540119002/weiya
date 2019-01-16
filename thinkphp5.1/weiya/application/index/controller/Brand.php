c37430776f882c3c71681072359ab61345c1662d
yangguanbao <yangguanbao@qq.com>
2019-01-16 16:06:46 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Brand.php
+++ thinkphp5.1/weiya/application/index/controller/Brand.php
@@ -5,6 +5,12 @@ class Brand extends \common\controller\Base{
     /**首页
      */
     public function index(){
+        $unlockingFooterCart = unlockingFooterCartConfig([15]);
+        $this->assign('unlockingFooterCart', $unlockingFooterCart);            
+        return $this->fetch();
+    }
+
+    public function edit(){
         $brandInfo = [
             'brand_type'=>1,
             'brand_name'=>'gaasg',
@@ -13,13 +19,6 @@ class Brand extends \common\controller\Base{
             'trademark_authorization'=>'temp/2019011615535288073.jpeg',
         ];
         $this->assign('info',$brandInfo);
-        $unlockingFooterCart = unlockingFooterCartConfig([15]);
-        $this->assign('unlockingFooterCart', $unlockingFooterCart);            
-        return $this->fetch();
-    }
-
-    public function edit(){
-
         return $this->fetch('info_tpl');
     }
 }
