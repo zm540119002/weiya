deedd878b449b985bb3699eaa7e381039bf6a637
zm <zm@example.com>
2019-01-16 17:02:05 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Brand.php
+++ thinkphp5.1/weiya/application/index/controller/Brand.php
@@ -11,6 +11,15 @@ class Brand extends \common\controller\Base{
     }
 
     public function edit(){
+        $data = input('post.');
+        $brandInfo = [
+            'brand_type'=>1,
+            'brand_name'=>'gaasg',
+            'brand_logo'=>'temp/2019011615535029895.jpeg',
+            'trademark_certificate'=>'temp/2019011615535288073.jpeg',
+            'trademark_authorization'=>'temp/2019011615535288073.jpeg',
+        ];
+        $this->assign('info',$data);
         return $this->fetch('info_tpl');
     }
 }
