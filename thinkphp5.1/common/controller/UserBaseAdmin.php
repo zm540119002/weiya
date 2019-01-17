b133f30a4ec8fcb23288f79330fcec525942b5f9
zhangmin <540119002@qq.com>
2019-01-17 14:00:28 +0800

    zm

--- thinkphp5.1/common/controller/UserBaseAdmin.php
+++ thinkphp5.1/common/controller/UserBaseAdmin.php
@@ -12,6 +12,7 @@ class UserBaseAdmin extends Base{
         parent::__construct();
         //判断是否登录
         $this->user = checkLogin();
+        print_r($this->user );exit;
         if (!$this->user) {
             if (request()->isAjax()) {
                 $this->success('异步登录失败',url($this->indexUrl),'no_login',0);
