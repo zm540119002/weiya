dea2cb0a52e02854dea7af5279af8a58ae9db674
zhangmin <540119002@qq.com>
2019-01-17 11:14:37 +0800

    zm

--- thinkphp5.1/weiya/config/all_node.php
+++ thinkphp5.1/weiya/config/all_node.php
@@ -8,6 +8,7 @@ return [
             'id'=>1,'name'=>'基本管理','type'=>1,
             'sub_menu' => [
                 ['id'=>2,'name'=>'账号管理','display'=>1,'controller'=>'User','action'=>'manage',],
+                ['id'=>3,'name'=>'个人信息','display'=>1,'controller'=>'User','action'=>'info',],
             ],
         ],
     ],
