5a71c724d4524e90f8f055723b84c8ae5f12bd7d
zhangmin <540119002@qq.com>
2019-01-18 14:26:39 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/CustomerService.php
+++ thinkphp5.1/weiya/application/index/controller/CustomerService.php
@@ -27,35 +27,38 @@ class CustomerService extends \common\controller\Base{
     public function sendMessage(){
         if(request()->isAjax()){
             $postData = input('post.');
+            $postData['to_user_id'] = 17;
             if($this->user['id']==$postData['to_user_id']){
                 return errorMsg('不能发给自己！');
             }
-            $modelChatMessage = new \common\model\ChatMessage();
-            $msgCreateTime = time();
-            $saveData = [
-                'from_id' => $this->user['id'],
-                'to_id' => $postData['to_user_id'],
-                'content' => $postData['content'],
-                'create_time' => $msgCreateTime,
-            ];
-            if(Gateway::isUidOnline($postData['to_user_id'])){
-                $saveData['send_sign'] = 1;
-            }
-            $res = $modelChatMessage->edit($saveData);
-            if($res['status']==0){
-                return errorMsg('保存失败！',$res);
-            }
-            if(Gateway::isUidOnline($postData['to_user_id'])){
-                $msg = [
-                    'type' => 'msg',
-                    'content' => $postData['content'],
+            if($this->user){
+                $modelChatMessage = new \common\model\ChatMessage();
+                $msgCreateTime = time();
+                $saveData = [
                     'from_id' => $this->user['id'],
-                    'from_name' => $this->user['name'],
-                    'avatar' => $this->user['avatar'],
-                    'create_time' => date('Y-m-d H:i',$msgCreateTime),
-                    'id' => $res['id'],
+                    'to_id' => $postData['to_user_id'],
+                    'content' => $postData['content'],
+                    'create_time' => $msgCreateTime,
                 ];
-                Gateway::sendToUid($postData['to_user_id'],json_encode($msg));
+                if(Gateway::isUidOnline($postData['to_user_id'])){
+                    $saveData['send_sign'] = 1;
+                }
+                $res = $modelChatMessage->edit($saveData);
+                if($res['status']==0){
+                    return errorMsg('保存失败！',$res);
+                }
+                if(Gateway::isUidOnline($postData['to_user_id'])){
+                    $msg = [
+                        'type' => 'msg',
+                        'content' => $postData['content'],
+                        'from_id' => $this->user['id'],
+                        'from_name' => $this->user['name'],
+                        'avatar' => $this->user['avatar'],
+                        'create_time' => date('Y-m-d H:i',$msgCreateTime),
+                        'id' => $res['id'],
+                    ];
+                    Gateway::sendToUid($postData['to_user_id'],json_encode($msg));
+                }
             }
             $postData['who'] = 'me';
             $postData['name'] = $this->user['name'];
@@ -71,15 +74,17 @@ class CustomerService extends \common\controller\Base{
     public function setMessageRead(){
         if(request()->isAjax()){
             $postData = input('post.');
-            $modelChatMessage = new \common\model\ChatMessage();
-            $where =
-                '`status` = 0 and `read` = 0 and id in (' . implode (",",$postData['messageIds']) .
-                ') and from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'];
-            $res = $modelChatMessage->where($where)->setField('read',1);
-            if($res==false){
-                return errorMsg('设置已读出错',$modelChatMessage->getError());
+            if($this->user){
+                $modelChatMessage = new \common\model\ChatMessage();
+                $where =
+                    '`status` = 0 and `read` = 0 and id in (' . implode (",",$postData['messageIds']) .
+                    ') and from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'];
+                $res = $modelChatMessage->where($where)->setField('read',1);
+                if($res==false){
+                    return errorMsg('设置已读出错',$modelChatMessage->getError());
+                }
+                return successMsg('成功！');
             }
-            return successMsg('成功！');
         }
     }
     /**客服聊天列表删除
@@ -87,16 +92,18 @@ class CustomerService extends \common\controller\Base{
     public function delCustomerMessage(){
         if(request()->isAjax()){
             $postData = input('post.');
-            $modelChatMessage = new \common\model\ChatMessage();
-            $where =
-                '`status` = 0 and id in (' . implode (",",$postData['messageIds']) .
-                ') and ((from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'] .') ' .
-                'or (from_id = ' . $this->user['id'] . ' and to_id = ' . $postData['from_id'] . '))';
-            $res = $modelChatMessage->where($where)->setField('status',2);
-            if($res==false){
-                return errorMsg('删除失败！',$modelChatMessage->getError());
+            if($this->user){
+                $modelChatMessage = new \common\model\ChatMessage();
+                $where =
+                    '`status` = 0 and id in (' . implode (",",$postData['messageIds']) .
+                    ') and ((from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'] .') ' .
+                    'or (from_id = ' . $this->user['id'] . ' and to_id = ' . $postData['from_id'] . '))';
+                $res = $modelChatMessage->where($where)->setField('status',2);
+                if($res==false){
+                    return errorMsg('删除失败！',$modelChatMessage->getError());
+                }
+                return successMsg('删除成功！');
             }
-            return successMsg('删除成功！');
         }
     }
 }
