18d9ca690b91e8e7cc216fa5ed844e2b3ccaba1c
zhangmin <540119002@qq.com>
2019-01-18 11:27:37 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/OnlineService.php
+++ thinkphp5.1/weiya/application/index/controller/OnlineService.php
@@ -1,48 +1,58 @@
 <?php
 namespace app\index\controller;
 
-class OnlineService extends \common\controller\UserBase{
+class OnlineService extends \common\controller\Base{
+    private $user = null;
+    public function __construct(){
+        parent::__construct();
+        //判断是否登录
+        $this->user = checkLogin();
+    }
     //首页
     public function index(){
         if(request()->isAjax()){
-            $modelChatMessage = new \common\model\ChatMessage();
-            $config = [
-                'field' => [
-                    'cm.id','cm.from_id','cm.to_id','cm.read','cm.content','cm.create_time',
-                    'u.name','u.avatar',
-                ],'join' => [
-                    ['common.user u','u.id = cm.from_id','left'],
-                ],'where' =>
-                    'u.status = 0 and cm.status = 0 and cm.type = 1 ' .
-                    'and ((cm.from_id = ' .$this->user['id'] . ' and cm.to_id = 17) ' .
-                    'or ( cm.from_id = 17' . ' and cm.to_id = ' .$this->user['id'] . '))'
-                ,'order' => [
-                    'cm.create_time'=>'desc',
-                ],'limit' => config('custom.chat_page_size'),
-            ];
-            $messages = $modelChatMessage->getList($config);
-            $messages = array_reverse($messages);
-            foreach ($messages as &$message){
-                if($this->user['id']==$message['from_id']){
-                    $message['who'] = 'me';
-                }else{
-                    $message['who'] = 'others';
+            if($this->user){
+                $modelChatMessage = new \common\model\ChatMessage();
+                $config = [
+                    'field' => [
+                        'cm.id','cm.from_id','cm.to_id','cm.read','cm.content','cm.create_time',
+                        'u.name','u.avatar',
+                    ],'join' => [
+                        ['common.user u','u.id = cm.from_id','left'],
+                    ],'where' =>
+                        'u.status = 0 and cm.status = 0 and cm.type = 1 ' .
+                        'and ((cm.from_id = ' .$this->user['id'] . ' and cm.to_id = 17) ' .
+                        'or ( cm.from_id = 17' . ' and cm.to_id = ' .$this->user['id'] . '))'
+                    ,'order' => [
+                        'cm.create_time'=>'desc',
+                    ],'limit' => config('custom.chat_page_size'),
+                ];
+                $messages = $modelChatMessage->getList($config);
+                $messages = array_reverse($messages);
+                foreach ($messages as &$message){
+                    if($this->user['id']==$message['from_id']){
+                        $message['who'] = 'me';
+                    }else{
+                        $message['who'] = 'others';
+                    }
                 }
+                $this->assign('list',$messages);
+                return view('list_tpl');
             }
-            $this->assign('list',$messages);
-            return view('list_tpl');
         }else{
-            $modelChatMessage = new \common\model\ChatMessage();
-            $config = [
-                'field' => [
-                    'count(id) num',
-                ],'where' =>
-                    'cm.status = 0 and cm.type = 1 and cm.read = 0 ' .
-                    'and (cm.from_id = 17 and cm.to_id = ' . $this->user['id'] . ') '
-                ,
-            ];
-            $unreadCount = $modelChatMessage->getList($config);
-            $this->assign('unreadCount',$unreadCount[0]['num']);
+            if($this->user){
+                $modelChatMessage = new \common\model\ChatMessage();
+                $config = [
+                    'field' => [
+                        'count(id) num',
+                    ],'where' =>
+                        'cm.status = 0 and cm.type = 1 and cm.read = 0 ' .
+                        'and (cm.from_id = 17 and cm.to_id = ' . $this->user['id'] . ') '
+                    ,
+                ];
+                $unreadCount = $modelChatMessage->getList($config);
+                $this->assign('unreadCount',$unreadCount[0]['num']);
+            }
             return $this->fetch();
         }
     }
