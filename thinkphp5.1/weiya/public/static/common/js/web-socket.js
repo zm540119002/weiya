15650af89df365372daac3bd1b23359be838aa1b
zhangmin <540119002@qq.com>
2019-01-18 13:56:02 +0800

    zm

--- thinkphp5.1/weiya/public/static/common/js/web-socket.js
+++ thinkphp5.1/weiya/public/static/common/js/web-socket.js
@@ -2,13 +2,16 @@ ws = new WebSocket("wss://www.worldview.com.cn:8282");
 ws.onopen = function(e){
     console.log('open');
 };
-console.log(loginSign);
 // 服务端主动推送消息时会触发这里的onmessage
 ws.onmessage = function(e){
     var data =  JSON.parse(e.data);
     var type = data.type || '';
     switch(type){
         case 'init':
+            //未登录则返回
+            if(!loginSign){
+                return false;
+            }
             // Events.php中返回的init类型的消息，将client_id发给后台进行uid绑定
             // 利用jquery发起ajax请求，将client_id发给后端进行uid绑定
             var postData = {client_id: data.client_id};
@@ -28,8 +31,6 @@ ws.onmessage = function(e){
                     $('.loading').hide();
                     if(msg.status==0){
                         dialog.error(msg.info);
-                    }else if(msg.code==1 && msg.data=='no_login'){
-                        //未登录
                     }else if(msg.code==1){
                         //已登录
                     }
