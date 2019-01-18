4656582f96b885f5c256e27843f52f74ed4529fe
zhangmin <540119002@qq.com>
2019-01-18 15:34:18 +0800

    zm

--- thinkphp5.1/weiya/application/index_admin/controller/CustomClient.php
+++ thinkphp5.1/weiya/application/index_admin/controller/CustomClient.php
@@ -1,67 +1,59 @@
 <?php
 namespace app\index_admin\controller;
 
-class CustomClient extends Base{
-    private $user = null;
-    public function __construct(){
-        parent::__construct();
-        //判断是否登录
-        $this->user = checkLogin();
-    }
+class CustomClient extends \common\controller\UserBase{
     /**售前
      */
     public function beforeSale(){
         if(request()->isAjax()){
-            if($this->user){
-                $modelChatMessage = new \common\model\ChatMessage();
+            $modelChatMessage = new \common\model\ChatMessage();
+            $config = [
+                'field' => [
+                    'cm.from_id','cm.to_id',
+                    'u.name','u.avatar',
+                ],'join' => [
+                    ['common.user u','u.id = cm.from_id','left'],
+                ],'where' => [
+                    ['u.status','=',0],
+                    ['cm.status','=',0],
+                    ['cm.type','=',1],
+                    ['cm.to_id','=',$this->user['id']],
+                ],'group' => 'cm.from_id'
+                ,
+            ];
+            $fromUserList = $modelChatMessage->getList($config);
+            foreach ($fromUserList as &$fromUser){
                 $config = [
                     'field' => [
-                        'cm.from_id','cm.to_id',
+                        'cm.id','cm.from_id','cm.to_id','cm.read','cm.content','cm.create_time',
                         'u.name','u.avatar',
                     ],'join' => [
                         ['common.user u','u.id = cm.from_id','left'],
-                    ],'where' => [
-                        ['u.status','=',0],
-                        ['cm.status','=',0],
-                        ['cm.type','=',1],
-                        ['cm.to_id','=',$this->user['id']],
-                    ],'group' => 'cm.from_id'
-                    ,
+                    ],'where' =>
+                        'u.status = 0 and cm.status = 0 and cm.type = 1 ' .
+                        'and ((cm.from_id = ' . $fromUser['from_id'] . ' and cm.to_id = ' . $this->user['id'] .') ' .
+                        'or ( cm.from_id = ' . $this->user['id'] . ' and cm.to_id = ' . $fromUser['from_id'] . '))'
+                    ,'order' => [
+                        'cm.create_time'=>'desc',
+                    ],'limit' => config('custom.chat_page_size'),
                 ];
-                $fromUserList = $modelChatMessage->getList($config);
-                foreach ($fromUserList as &$fromUser){
-                    $config = [
-                        'field' => [
-                            'cm.id','cm.from_id','cm.to_id','cm.read','cm.content','cm.create_time',
-                            'u.name','u.avatar',
-                        ],'join' => [
-                            ['common.user u','u.id = cm.from_id','left'],
-                        ],'where' =>
-                            'u.status = 0 and cm.status = 0 and cm.type = 1 ' .
-                            'and ((cm.from_id = ' . $fromUser['from_id'] . ' and cm.to_id = ' . $this->user['id'] .') ' .
-                            'or ( cm.from_id = ' . $this->user['id'] . ' and cm.to_id = ' . $fromUser['from_id'] . '))'
-                        ,'order' => [
-                            'cm.create_time'=>'desc',
-                        ],'limit' => config('custom.chat_page_size'),
-                    ];
-                    $fromUser['messages'] = array_reverse($modelChatMessage->getList($config));
-                }
-                foreach ($fromUserList as &$fromUser){
-                    $fromUser['unreadCount'] = 0;
-                    foreach ($fromUser['messages'] as &$message){
-                        if($fromUser['from_id']==$message['from_id']){
-                            if($message['read']==0){
-                                $fromUser['unreadCount'] ++;
-                            }
-                            $message['who'] = 'others';
-                        }else{
-                            $message['who'] = 'me';
+                $fromUser['messages'] = array_reverse($modelChatMessage->getList($config));
+            }
+            foreach ($fromUserList as &$fromUser){
+                $fromUser['unreadCount'] = 0;
+                foreach ($fromUser['messages'] as &$message){
+                    if($fromUser['from_id']==$message['from_id']){
+                        if($message['read']==0){
+                            $fromUser['unreadCount'] ++;
                         }
+                        $message['who'] = 'others';
+                    }else{
+                        $message['who'] = 'me';
                     }
                 }
-                $this->assign('list',$fromUserList);
-                return view('list_tpl');
             }
+            $this->assign('list',$fromUserList);
+            return view('list_tpl');
         }else{
             return $this->fetch();
         }
