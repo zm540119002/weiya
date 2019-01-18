7d932c6e7adf3c42fcf78e04a011737721c4eb20
zhangmin <540119002@qq.com>
2019-01-18 13:51:28 +0800

    zm

--- thinkphp5.1/weiya/public/static/common/js/web-socket.js
+++ thinkphp5.1/weiya/public/static/common/js/web-socket.js
@@ -2,7 +2,6 @@ ws = new WebSocket("wss://www.worldview.com.cn:8282");
 ws.onopen = function(e){
     console.log('open');
 };
-var userId = '{$userId}';
 console.log(userId);
 // 服务端主动推送消息时会触发这里的onmessage
 ws.onmessage = function(e){
