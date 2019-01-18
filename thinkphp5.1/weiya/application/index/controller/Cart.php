57f5ac07c23b92c0264d5bdd0200dc4310151f8c
yangguanbao <yangguanbao@qq.com>
2019-01-18 15:24:45 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Cart.php
+++ thinkphp5.1/weiya/application/index/controller/Cart.php
@@ -23,6 +23,7 @@ class Cart extends \common\controller\UserBase{
             return errorMsg('请求方式错误');
         }
         $goodsList = input('post.goodsList/a');
+        print_r($goodsList);
         if(empty($goodsList)){
             return errorMsg('没有数据');
         }
@@ -40,25 +41,32 @@ class Cart extends \common\controller\UserBase{
             //假定没找到
             $find = false;
             foreach ($cartList as $cart){
-                if($goods['foreign_id'] == $cart['foreign_id'] && $goods['buy_type'] == $cart['buy_type'] ){//找到了，则更新记录
+                if($goods['foreign_id'] == $cart['foreign_id'] && $goods['buy_type'] == $cart['buy_type'] && $goods['brand_name'] == $cart['brand_name'] ){//找到了，则更新记录
                     $find = true;
                     $data = [
                         'user_id' => $this->user['id'],
                         'id' => $cart['id'],
                         'foreign_id' => $cart['foreign_id'],
                         'buy_type' => $cart['buy_type'],
-                        'num' => $goods['num'] + $cart['num']
+                        'num' => $goods['num'] + $cart['num'],
+                        'brand_name' => $cart['brand_name'],
+                        'brand_id' => $cart['brand_id'],
+                        'update_time'=>time(),
                     ];
                     $updateData[] = $data;
                 }
             }
             if(!$find){//如果没找到，则新增
-                $data = [];
-                $data['user_id'] = $this->user['id'];
-                $data['foreign_id'] = $goods['foreign_id'];
-                $data['num'] = $goods['num'];
-                $data['buy_type'] = $goods['buy_type'];
-                $data['create_time'] = time();
+                print_r($goods);exit;
+                $data = [
+                    'user_id' => $this->user['id'],
+                    'foreign_id' => $goods['foreign_id'],
+                    'buy_type' =>$goods['buy_type'],
+                    'num' =>$goods['num'],
+                    'brand_name' => $goods['brand_name'],
+                    'brand_id' => $goods['brand_id'],
+                    'create_time'=>time(),
+                ];
                 $addData[] = $data;
             }
         }
--- thinkphp5.1/weiya/public/static/weiya/js/footerCart.js
+++ thinkphp5.1/weiya/public/static/weiya/js/footerCart.js
@@ -312,6 +312,9 @@ $(function () {
         });
     });
 
+    //再次购买
+    
+
 
     //购物车弹窗 样品购买
     var goodsInfoLayer=$('#goodsInfoLayer').html();
