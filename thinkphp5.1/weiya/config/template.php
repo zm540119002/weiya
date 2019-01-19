b0fddce4b141d1f38107a385da5200e8307e1bc9
zhangmin <540119002@qq.com>
2019-01-19 16:17:56 +0800

    zm

--- thinkphp5.1/weiya/config/template.php
+++ thinkphp5.1/weiya/config/template.php
@@ -56,10 +56,10 @@ return [
         'hui_img' => Request::domain() .'/static/h-ui/images',
         'hui_lib' => Request::domain() .'/static/h-ui.lib',
         //h-ui.admin资源路径
-        'hui_admin2_js' => Request::domain() .'/static/h-ui.admin/js',
-        'hui_admin2_css' => Request::domain() .'/static/h-ui.admin/css',
-        'hui_admin2_img' => Request::domain() .'/static/h-ui.admin/images',
-        'hui_admin2_skin' => Request::domain() .'/static/h-ui.admin/skin',
+        'hui_admin_js' => Request::domain() .'/static/h-ui.admin/js',
+        'hui_admin_css' => Request::domain() .'/static/h-ui.admin/css',
+        'hui_admin_img' => Request::domain() .'/static/h-ui.admin/images',
+        'hui_admin_skin' => Request::domain() .'/static/h-ui.admin/skin',
         //上传路径
         'public_uploads' => Request::domain() .'/uploads',
     ],
