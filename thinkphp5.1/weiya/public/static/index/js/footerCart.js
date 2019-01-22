3cb29d048b92f1398198d6cbde5fdf159203403a
zhuguangfu <zhuguangfu@qq.com>
2019-01-22 16:55:17 +0800

    zgf

--- thinkphp5.1/weiya/public/static/index/js/footerCart.js
+++ thinkphp5.1/weiya/public/static/index/js/footerCart.js
@@ -374,8 +374,6 @@ $(function () {
             return false
         }
         postData.goodsList=goodsList;
-        console.log(postData);
-        return false;
         _This.addClass("disabled");//防止重复提交
         generateOrder(postData,_This);
     });
