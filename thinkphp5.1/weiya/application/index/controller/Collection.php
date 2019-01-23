4a8bca401974b3b9bcb4f4c4ce7a1006b7fa7bce
yangguanbao <yangguanbao@qq.com>
2019-01-23 14:39:06 +0800

    ygb

--- thinkphp5.1/weiya/application/index/controller/Collection.php
+++ thinkphp5.1/weiya/application/index/controller/Collection.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\controller;
 
-class Collection extends \common\controller\Base{
+class Collection extends \common\controller\UserBase{
     /**首页
      */
     public function index(){
@@ -10,7 +10,40 @@ class Collection extends \common\controller\Base{
         return $this->fetch();
     }
 
-    public function Collection(){
+    /**
+     * 收藏
+     */
+    public function collect(){
+        if(!request()->isAjax()){
+            return errorMsg('请求方式错误');
+        }
+        $goodsId = input('post.goods_id/d');
+        if(!$goodsId){
+            return errorMsg('参数错误');
+        }
+        $model = new \app\index\model\Collection();
+        $config = [
+            'where'=>[
+                ['user_id','=',$this->user['id']],
+                ['goods_id','=',$goodsId],
+                ['status','=',0]
+            ]
+        ];
+        $info = $model -> getInfo($config);
+        if(count($info)){
+            return successMsg('收藏成功');
+        }
+        $data = [
+            'user_id'=>$this->user['id'],
+            'goods_id'=>$goodsId,
+            'create_time'=>time(),
+        ];
+        $result = $model -> isUpdate(false) -> save($data);
+        if($result){
+            return successMsg('收藏成功');
+        }else{
+            return errorMsg('收藏失败');
+        }
         
     }
 }
