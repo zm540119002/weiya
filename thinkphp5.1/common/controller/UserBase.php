932e02f05b52a4921c95f47781c155cbc723346f
yangguanbao <yangguanbao@qq.com>
2019-01-24 16:01:41 +0800

    ygb

--- thinkphp5.1/common/controller/UserBase.php
+++ thinkphp5.1/common/controller/UserBase.php
@@ -37,7 +37,32 @@ class UserBase extends Base{
                 //临时相对路径
                 $tempRelativePath = config('upload_dir.temp_path');
                 $newFile = $uploadPath.$tempRelativePath.generateSN(15)."png";
-                $newFile = saveImageFromHttp($weiXinUserInfo['headimgurl'],$newFile);
+
+                $header = array(
+                    'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:45.0) Gecko/20100101 Firefox/45.0',
+                    'Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+                    'Accept-Encoding: gzip, deflate',);
+                $url=$weiXinUserInfo['headimgurl'];
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                $data = curl_exec($curl);$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);curl_close($curl);
+                if ($code == 200) {//把URL格式的图片转成base64_encode格式的！
+                    $imgBase64Code = "data:image/jpeg;base64," . base64_encode($data);
+                }
+                $img_content=$imgBase64Code;//图片内容
+                //echo $img_content;exit;
+                if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $img_content, $result))
+                {
+                    $type = $result[2];//得到图片类型png?jpg?gif?
+                    $newFile = $uploadPath.$tempRelativePath.generateSN(15)."{$type}";
+                    if (file_put_contents($newFile, base64_decode(str_replace($result[1], '', $img_content))))
+                    {  echo '新文件保存成功：', $newFile; }}
+
+
                 print_r($newFile);exit;
                 $data = [
                     'id'=>$this->user['id'],
