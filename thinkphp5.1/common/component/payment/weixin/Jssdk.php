4109825e1cbed4ff2a0fa502c4cba440e84ad144
yangguanbao <yangguanbao@qq.com>
2019-01-24 12:37:14 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/Jssdk.php
+++ thinkphp5.1/common/component/payment/weixin/Jssdk.php
@@ -11,7 +11,7 @@ class Jssdk {
     $this->appId = $appId;
     $this->appSecret = $appSecret;
     $this->path = __DIR__ . 'Jssdk.php/';
-    $data = json_decode($this->get_php_file("access_token.php"));
+    $data = json_decode($this->get_php_file("a.php"));
     if ($data->expire_time < time()) {
       // 如果是企业号用以下URL获取access_token
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
@@ -21,7 +21,7 @@ class Jssdk {
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
-        $this->set_php_file("access_token.php", json_encode($data));
+        $this->set_php_file("a.php", json_encode($data));
       }
     } else {
       $access_token = $data->access_token;
