17abfda71d50b94cf6fd779649c8c32889242416
yangguanbao <yangguanbao@qq.com>
2019-01-24 13:30:51 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/getPayOpenId.php
+++ thinkphp5.1/common/component/payment/weixin/getPayOpenId.php
@@ -103,7 +103,7 @@ class getPayOpenId {
     if (!isset($_GET['code'])){
       //触发微信返回code码
 //			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
-      $baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+      $baseUrl = urlencode('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
       $url = $this->__CreateOauthUrlForCode($baseUrl);
       header("Location: $url");
       exit();
