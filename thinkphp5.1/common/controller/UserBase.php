8d64b98c7b740f50b2d4430697e0d7552091ebca
yangguanbao <yangguanbao@qq.com>
2019-01-24 10:04:23 +0800

    ygb

--- thinkphp5.1/common/controller/UserBase.php
+++ thinkphp5.1/common/controller/UserBase.php
@@ -27,6 +27,9 @@ class UserBase extends Base{
                 session('pay_open_id',$payOpenId);
             }
             $mineTools = new \common\component\payment\weixin\Jssdk(config('weiya_weixin.appid'), config('weiya_weixin.appsecret'));
+            print_r(config('weiya_weixin.appid'));
+            print_r(config('weiya_weixin.appsecret'));exit;
+
             $weiXinUserInfo = $mineTools->get_user_info($mineTools->getOpenid());
             print_r($weiXinUserInfo);exit;
         }
