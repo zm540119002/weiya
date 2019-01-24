49f619b47273d4439c87e420a9d3209461c185e8
yangguanbao <yangguanbao@qq.com>
2019-01-24 15:29:47 +0800

    ygb

--- thinkphp5.1/common/model/UserCenter.php
+++ thinkphp5.1/common/model/UserCenter.php
@@ -167,7 +167,7 @@ class UserCenter extends Base {
 		);
 		$field = array(
 			'id','name','nickname','mobile_phone','status','type','password','avatar',
-			'sex','salt','birthday','last_login_time','role_id'
+			'sex','salt','birthday','last_login_time','role_id','weiya_openid'
 		);
 		$user = $this->field($field)->where($where)->find();
 		if(!count($user)) {
