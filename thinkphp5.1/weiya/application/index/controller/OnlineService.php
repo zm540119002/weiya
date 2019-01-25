b4fcd1c89703287e3fe2e5b27728bef7b4e2c073
zm <zm@example.com>
2019-01-25 17:25:55 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/OnlineService.php
+++ thinkphp5.1/weiya/application/index/controller/OnlineService.php
@@ -10,6 +10,7 @@ class OnlineService extends \common\controller\Base{
     }
     //首页
     public function index(){
+        print_r(config('upload_dir.temp_path'));exit;
         if(request()->isAjax()){
             if($this->user){
                 $modelChatMessage = new \common\model\ChatMessage();
