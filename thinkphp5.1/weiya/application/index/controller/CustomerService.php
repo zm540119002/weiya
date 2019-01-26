502fb855863f51d93edf726ea34cf28336bd39d7
zhangmin <540119002@qq.com>
2019-01-26 17:01:00 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/CustomerService.php
+++ thinkphp5.1/weiya/application/index/controller/CustomerService.php
@@ -102,7 +102,7 @@ class CustomerService extends \common\controller\Base{
                     '`status` = 0 and `read` = 0 and `id` in (' . implode (",",$postData['messageIds']) .
                     ') and from_id = ' . $postData['from_id'] . ' and to_id = ' . $this->user['id'];
                 $res = $modelChatMessage->where($where)->setField('read',1);
-                if($res==false){
+                if($res===false){
                     return errorMsg('设置已读出错',$modelChatMessage->getError());
                 }
                 return successMsg('成功！');
