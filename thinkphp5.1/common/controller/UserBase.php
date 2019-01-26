541540a32f42af9cf94eb69b2c49cd77a9905f6e
yangguanbao <yangguanbao@qq.com>
2019-01-26 16:19:02 +0800

    ygb

--- thinkphp5.1/common/controller/UserBase.php
+++ thinkphp5.1/common/controller/UserBase.php
@@ -19,33 +19,33 @@ class UserBase extends Base{
                 $this->error(config('custom.error_login'),url($this->loginUrl));
             }
         }
-        //判断是否为微信浏览器 没有
-        if(isWxBrowser() && !request()->isAjax()) {
-            if(!$this -> user['weiya_openid']){
-                $weiXinUserInfo = session('weiXinUserInfo');
-                //临时相对路径
-                $tempRelativePath = config('upload_dir.user_avatar');
-                $weiXinAvatarUrl = $weiXinUserInfo['headimgurl'];
-                $avatar = saveImageFromHttp($weiXinAvatarUrl,$tempRelativePath);
-                $data = [
-                    'id'=>$this->user['id'],
-                    'name'=>$weiXinUserInfo['nickname'],
-                    'avatar'=>$avatar,
-                    'weiya_openid'=>$weiXinUserInfo['openid'],
-                ];
-                if($this->user['avatar']){
-                    unset($data['avatar']);
-                }
-                if($this->user['name']){
-                    unset($data['name']);
-                }
-                $userModel = new \common\model\User();
-                $result = $userModel->isUpdate(true)->save($data);
-                if( false === $result){
-                    return errorMsg('添加微信信息失败');
-                }
-            }
-        }
+//        //判断是否为微信浏览器 没有
+//        if(isWxBrowser() && !request()->isAjax()) {
+//            if(!$this -> user['weiya_openid']){
+//                $weiXinUserInfo = session('weiXinUserInfo');
+//                //临时相对路径
+//                $tempRelativePath = config('upload_dir.user_avatar');
+//                $weiXinAvatarUrl = $weiXinUserInfo['headimgurl'];
+//                $avatar = saveImageFromHttp($weiXinAvatarUrl,$tempRelativePath);
+//                $data = [
+//                    'id'=>$this->user['id'],
+//                    'name'=>$weiXinUserInfo['nickname'],
+//                    'avatar'=>$avatar,
+//                    'weiya_openid'=>$weiXinUserInfo['openid'],
+//                ];
+//                if($this->user['avatar']){
+//                    unset($data['avatar']);
+//                }
+//                if($this->user['name']){
+//                    unset($data['name']);
+//                }
+//                $userModel = new \common\model\User();
+//                $result = $userModel->isUpdate(true)->save($data);
+//                if( false === $result){
+//                    return errorMsg('添加微信信息失败');
+//                }
+//            }
+//        }
     }
     
 }
