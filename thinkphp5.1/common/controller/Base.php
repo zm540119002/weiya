d5b8d86a974791434f3a2e4131cee23c9d92cef7
yangguanbao <yangguanbao@qq.com>
2019-02-12 17:27:33 +0800

    ygb

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -23,8 +23,7 @@ class Base extends \think\Controller{
             $weiXinUserInfo =  session('weiXinUserInfo');
             if(empty($weiXinUserInfo)){
                 $mineTools = new \common\component\payment\weixin\Jssdk(config('weiya_weixin.appid'), config('weiya_weixin.appsecret'));
-//                $weiXinUserInfo = $mineTools->getOauthUserInfo();
-                $weiXinUserInfo = $mineTools->getUserInfo();
+                $weiXinUserInfo = $mineTools->getOauthUserInfo();
                 session('weiXinUserInfo',$weiXinUserInfo);
             }
             $this -> assign('weiXinUserInfo',$weiXinUserInfo);
