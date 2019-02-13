dba8d3d6df5274e33af039aa9beae033c1479025
zm <zm@example.com>
2019-02-13 11:33:00 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Collection.php
+++ thinkphp5.1/weiya/application/index/controller/Collection.php
@@ -27,6 +27,8 @@ class Collection extends \common\controller\UserBase{
                 ['user_id','=',$this->user['id']],
                 ['goods_id','=',$goodsId],
                 ['status','=',0]
+            ] ,'field'=>[
+                'id'
             ]
         ];
         $info = $model -> getInfo($config);
@@ -78,8 +80,10 @@ class Collection extends \common\controller\UserBase{
         if($keyword) {
             $config['where'][] = ['o.name', 'like', '%' . trim($keyword) . '%'];
         }
+
         $list = $model -> pageQuery($config);
-        $this->assign('currentPage',$list->currentPage());
+        $currentPage = input('get.page/d');
+        $this->assign('currentPage',$currentPage);
         $this->assign('list',$list);
         if(isset($_GET['pageType'])){
             $pageType = $_GET['pageType'];
@@ -96,7 +100,7 @@ class Collection extends \common\controller\UserBase{
         $model = new \app\index\model\Collection();
         $condition = [
             ['user_id','=',$this->user['id']],
-            ['id','in',$ids],
+            ['goods_id','in',$ids],
         ];
         $result = $model -> del($condition);
         if($result['status']){
