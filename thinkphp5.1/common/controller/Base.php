53478962bc829ed1181697ea41fc1a808f8083ae
zhangmin <540119002@qq.com>
2019-02-27 10:07:05 +0800

    zm

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -18,7 +18,8 @@ class Base extends \think\Controller{
             $this->host . $_SERVER['HTTP_REFERER']);
         //去到页面跟返回跳转不一样，前端传参returnUrl
         session('returnUrl',input('get.returnUrl','')?:input('post.returnUrl',''));
-
+        print_r(123);
+        exit;
         if(isWxBrowser() && !request()->isAjax()) {//判断是否为微信浏览器
             $weiXinUserInfo =  session('weiXinUserInfo');
             print_r(123);
