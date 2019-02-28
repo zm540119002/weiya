f20063cde7a3e5f8693cf4eaad4780c0dc7ae2fb
yangguanbao <yangguanbao@qq.com>
2019-02-28 10:38:53 +0800

    ygb

--- thinkphp5.1/common/component/payment/weixin/Jssdk.php
+++ thinkphp5.1/common/component/payment/weixin/Jssdk.php
@@ -1179,7 +1179,7 @@ class Jssdk {
   {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
     if (!empty($data)){
       curl_setopt($curl, CURLOPT_POST, 1);
