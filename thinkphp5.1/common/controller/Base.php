4b1b06488baf024134ce9839b9ac0f1be9587504
zm <zm@example.com>
2019-02-28 15:43:03 +0800

    zm

--- thinkphp5.1/common/controller/Base.php
+++ thinkphp5.1/common/controller/Base.php
@@ -5,15 +5,19 @@ use think\facade\Session;
 /**基于公共基础控制器
  */
 class Base extends \think\Controller{
+    protected $http_type = null;
     protected $host = null;
+    protected $request_uri = null;
     public function __construct(){
         parent::__construct();
         //登录验证后跳转回原验证发起页
-        $this->host = http_type() . (isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] :
+        $this->http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
+                && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+        $this->host = $this->http_type . (isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] :
             (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ''));
+        $this->request_uri = $_SERVER['REQUEST_URI'] ? $this->host . $_SERVER['REQUEST_URI'] :$this->host . $_SERVER['HTTP_REFERER'];
         //去到页面跟返回跳转一样，前端不用传参
-        session('backUrl',$_SERVER['REQUEST_URI'] ? $this->host . $_SERVER['REQUEST_URI'] :
-            $this->host . $_SERVER['HTTP_REFERER']);
+        session('backUrl',$this->request_uri);
         //去到页面跟返回跳转不一样，前端传参returnUrl
         session('returnUrl',input('get.returnUrl','')?:input('post.returnUrl',''));
         if(isWxBrowser() && !request()->isAjax()) {//判断是否为微信浏览器
