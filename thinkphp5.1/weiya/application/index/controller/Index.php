3f6579d63eadff6933a34280eee9e991b4701329
yangguanbao <yangguanbao@qq.com>
2019-03-19 16:28:47 +0800

    ''uagb

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -4,6 +4,15 @@ class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
+
+        $urlToken = "https://open.api.clife.cn/apigateway/commons/clife-open-api-app/cloud/token";
+        $data = [
+            'appId'=>'31316',
+            'appSecret'=>'0ab242fc269f4119bc9f4ad9e6884332',
+            'timestamp'=>time(),
+        ];
+        $res = $this->http_request($urlToken,$data);
+        print_r($res);exit;
 //        print_r('系统维护中，请稍后。。。');
 //        exit;
         //获取商品的分类
@@ -51,4 +60,22 @@ class Index extends \common\controller\Base{
         $this ->assign('projectList',$projectList);
         return $this->fetch();
     }
+
+    //HTTP请求（支持HTTP/HTTPS，支持GET/POST）
+    protected function http_request($url, $data = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)){
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+    }
 }
+
