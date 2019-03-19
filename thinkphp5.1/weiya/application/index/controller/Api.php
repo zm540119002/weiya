4e11618f2914b7be8736ef68bbcc399b821a5f9a
yangguanbao <yangguanbao@qq.com>
2019-03-19 17:14:28 +0800

    ''uagb

--- thinkphp5.1/weiya/application/index/controller/Api.php
+++ thinkphp5.1/weiya/application/index/controller/Api.php
@@ -1,64 +1,19 @@
 <?php
 namespace app\index\controller;
 
-class Api extends \common\controller\UserBase{
+class Api extends \common\controller\Base{
     /**首页
      */
     public function index(){
-
         $urlToken = "https://open.api.clife.cn/apigateway/commons/clife-open-api-app/cloud/token";
         $data = [
-            'appId'=>'31316',
-            'appSecret'=>'0ab242fc269f4119bc9f4ad9e6884332',
+            'appid'=>'31316',
+            'appsecret'=>'0ab242fc269f4119bc9f4ad9e6884332',
             'timestamp'=>time(),
         ];
         $res = $this->http_request($urlToken,$data);
+        $res =  json_decode($res, true);
         print_r($res);exit;
-//        print_r('系统维护中，请稍后。。。');
-//        exit;
-        //获取商品的分类
-        $modelGoodsCategory = new \app\index\model\GoodsCategory();
-        $config =[
-            'where' => [
-                ['status', '=', 0],
-                ['level','=',1],
-            ], 'order'=>[
-                'sort'=>'desc',
-                'id'=>'desc'
-            ],  'limit'=>'7'
-        ];
-        $categoryList  = $modelGoodsCategory->getList($config);
-        $this ->assign('categoryList',$categoryList);
-        //获取精选的6个 场景
-        $modelScene = new \app\index\model\Scene();
-        $config =[
-            'where' => [
-                ['status', '=', 0],
-                ['shelf_status','=',3],
-                ['is_selection','=',1],
-            ], 'order'=>[
-                'sort'=>'desc',
-                'id'=>'desc'
-            ],  'limit'=>'6'
-        ];
-        $sceneList  = $modelScene->getList($config);
-        $this ->assign('sceneList',$sceneList);
-
-        //获取精选的10个项目
-        $modelProject = new \app\index\model\Project();
-        $config =[
-            'where' => [
-                ['status', '=', 0],
-                ['shelf_status','=',3],
-                ['is_selection','=',1],
-            ], 'order'=>[
-                'sort'=>'desc',
-                'id'=>'desc'
-            ],  'limit'=>'6'
-        ];
-        $projectList  = $modelProject->getList($config);
-        $this ->assign('projectList',$projectList);
-        return $this->fetch();
     }
 
     //HTTP请求（支持HTTP/HTTPS，支持GET/POST）
