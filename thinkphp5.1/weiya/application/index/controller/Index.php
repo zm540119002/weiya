6c58e202c361abc0465bc62ab4c16722ef198c7c
zm <zm@example.com>
2019-03-22 11:17:21 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Index.php
+++ thinkphp5.1/weiya/application/index/controller/Index.php
@@ -1,15 +1,11 @@
 <?php
 namespace app\index\controller;
-use \common\component\curl\Curl;
 class Index extends \common\controller\Base{
     /**首页
      */
     public function index(){
-        $curl = new Curl();
-        print_r($curl);
-
 //        print_r('系统维护中，请稍后。。。');
-        exit;
+//        exit;
         //获取商品的分类
         $modelGoodsCategory = new \app\index\model\GoodsCategory();
         $config =[
