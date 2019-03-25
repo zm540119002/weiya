0cb6410ee2dd4036d28c67808ca1a74ebe0fdef7
zhangmin <540119002@qq.com>
2019-03-25 11:38:36 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Api.php
+++ thinkphp5.1/weiya/application/index/controller/Api.php
@@ -32,9 +32,9 @@ class Api extends \common\controller\Base{
         } else {
             $res = json_encode($curl->response->data);
             print_r($res);
-            foreach ($curl->response->data as $key=>$val){
-                echo "<br/>" . $key;
-            }
+//            foreach ($curl->response->data as $key=>$val){
+//                echo "<br/>" . $key;
+//            }
         }
     }
 }
