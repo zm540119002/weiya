a0ba5cd28d98ba6f79c767978db8d1254c32d772
zm <zm@example.com>
2019-03-25 16:46:33 +0800

    zm

---
+++ thinkphp5.1/weiya/config/clife.php
@@ -0,0 +1,93 @@
+<?php
+return [
+    'eyeshape' => [
+        ['type' => ['eyelid'=>1,'narrow'=>1,'updown'=>1],'explain' => '铜铃眼',],
+        ['type' => ['eyelid'=>1,'narrow'=>1,'updown'=>2],'explain' => '睡龙眼',],
+        ['type' => ['eyelid'=>2,'narrow'=>2,'updown'=>1],'explain' => '丹凤眼',],
+        ['type' => ['eyelid'=>2,'narrow'=>2,'updown'=>2],'explain' => '睡凤眼',],
+        ['type' => ['eyelid'=>1,'narrow'=>2,'updown'=>1],'explain' => '瑞凤眼',],
+        ['type' => ['eyelid'=>1,'narrow'=>2,'updown'=>2],'explain' => '月牙眼',],
+        ['type' => ['eyelid'=>2,'narrow'=>3,'updown'=>1],'explain' => '桃花眼',],
+        ['type' => ['eyelid'=>2,'narrow'=>3,'updown'=>2],'explain' => '柳叶眼',],
+        ['type' => ['eyelid'=>1,'narrow'=>3,'updown'=>1],'explain' => '狐媚眼',],
+        ['type' => ['eyelid'=>1,'narrow'=>3,'updown'=>2],'explain' => '孔雀眼',],
+    ],'blackHeadLevel'=>[
+        1 => '无',
+        2 => '极少',
+        3 => '轻度',
+        4 => '中度',
+        5 => '重度',
+    ],'poreLevel'=>[
+        1 => '紧致',
+        2 => '轻度',
+        3 => '中度',
+        4 => '重度',
+    ],'faceshape'=>[
+        'H' => '心形脸',
+        'O' => '鹅蛋脸',
+        'S' => '方脸',
+        'R' => '圆脸',
+    ],'acneTypeId'=>[
+        1 => '红斑',
+        2 => '粉刺',
+        3 => '炎症性丘疹',
+        4 => '脓包',
+        5 => '凹陷性瘢痕',
+        6 => '结节',
+    ],'acneLevel'=>[
+        1 => '无',
+        2 => '轻度',
+        3 => '中度',
+        4 => '重度',
+    ],'facePart'=>[
+        0 => '脸部',
+        1 => '额头',
+        2 => '鼻子',
+        3 => '左脸',
+        4 => '右脸',
+        5 => '下颌',
+    ],'eyebrow'=>[
+        1 => '柳叶眉',
+        2 => '平直眉',
+        3 => '秋波眉',
+        4 => '秋娘眉',
+        5 => '双燕眉',
+        6 => '水弯眉',
+    ],'pouchLevel'=>[
+        1 => '无眼袋',
+        2 => '轻微',
+        3 => '严重',
+    ],'wrinkleTypeId'=>[
+        1 => '抬头纹',
+        2 => '法令纹',
+        3 => '泪沟',
+        4 => '笑肌断层',
+    ],'pigmentationTypeId'=>[
+        1 => '黑痣',
+        2 => '黄褐斑',
+        3 => '雀斑',
+        4 => '隐藏斑',
+    ],'className'=>[
+        1 => '滋润',
+        2 => '敏感性缺水',
+        3 => '油脂性缺水',
+        4 => '老化性缺水',
+    ],'sensitivityTypeId'=>[
+        1 => '耐受',
+        2 => '敏感',
+    ],'darkCircleType'=>[
+        1 => '无',
+        2 => '血管型',
+        3 => '色素型',
+    ],'darkCircleLevel'=>[
+        1 => '无',
+        2 => '轻微',
+        3 => '严重',
+    ],'darkCirclePosition'=>[
+        1 => '左眼',
+        2 => '右眼',
+    ],'fatGranuleTypeId'=>[
+        1 => '汗管瘤',
+        2 => '栗丘疹',
+    ],
+];
