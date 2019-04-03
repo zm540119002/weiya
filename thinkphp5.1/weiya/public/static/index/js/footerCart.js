b5d5d316ba46b430d2d8283dee3e32149de4e6a4
yangguanbao <yangguanbao@qq.com>
2019-04-03 16:42:32 +0800

    ygb

--- thinkphp5.1/weiya/public/static/index/js/footerCart.js
+++ thinkphp5.1/weiya/public/static/index/js/footerCart.js
@@ -287,6 +287,7 @@ $(function () {
     });
     //确认订单
     $('body').on('click','.confirm_order',function () {
+        alert(1122);
         _this = $(this);
         var consignee=$('.consigneeInfo input[name="layer_consignee"]').val();
         var mobile=$('.consigneeInfo input[name="layer_mobile"]').val();
@@ -343,6 +344,7 @@ $(function () {
                 if(data.status == 0){
 
                 }else if(data.status == 1){
+                    alert(11);
                     location.href = 'https://msy.meishangyun.com/' + 'index/Payment/toPay/order_sn/' + data.order_sn+'/system_id/1';
 
                 }
