80666425b5367ef71b38ecd3b222010c3c6a4c22
yangguanbao <yangguanbao@qq.com>
2019-04-13 13:37:32 +0800

    aa

--- thinkphp5.1/weiya/public/static/index/js/footerCart.js
+++ thinkphp5.1/weiya/public/static/index/js/footerCart.js
@@ -2,6 +2,7 @@
 function addCart(postData) {
     var url = module + 'Cart/addCart';
      var _this=postData._this;
+     console.log(123);
      console.log(_this);
      var lis=postData.lis;
     _this.addClass("nodisabled");//防止重复提交
