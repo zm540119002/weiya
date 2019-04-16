5e4b3151478c485e8da7c7d0020a660cab64b7b0
jing <2517187814@qq.com>
2019-04-16 10:25:59 +0800

    1

--- thinkphp5.1/api/public/static/common/js/common.js
+++ thinkphp5.1/api/public/static/common/js/common.js
@@ -327,26 +327,6 @@ $.fn.serializeObject = function() {
             o[this.name] = this.value || '';
         }
     });
-    //解决checkbox未选中时，没有序列化到对象中的代码
-    var checkboxes = $(this).find('input[type=checkbox]');
-    $.each(checkboxes, function () {
-        if (o[this.name]) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            if($(this).prop('checked')) {
-                o[this.name].push('1');
-            }else{
-                o[this.name].push('0');
-            }
-        }else{
-            if($(this).prop('checked')){
-                o[this.name] = '1';
-            }else{
-                o[this.name] = '0';
-            }
-        }
-    });
     return o;
 };
 //jquery 扩展
