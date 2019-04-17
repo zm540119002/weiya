4feac3cba3bb217d6211ba779bb7c4c097022663
zhangmin <540119002@qq.com>
2019-04-17 09:57:38 +0800

    zm

--- thinkphp5.1/api/config/template.php
+++ thinkphp5.1/api/config/template.php
@@ -34,6 +34,7 @@ return [
         'api_common_js' => 'https://api.worldview.com.cn/static/common/js',
         'api_common_css' => 'https://api.worldview.com.cn/static/common/css',
         'api_common_img' => 'https://api.worldview.com.cn/static/common/img',
+        'api_common_uploads' => 'https://api.worldview.com.cn/uploads',
         //common  资源路径，此处不能命名成common_js，与下面的api_common_js冲突
         'public_js' => Request::domain() .'/static/common/js',
         'public_css' => Request::domain() .'/static/common/css',
