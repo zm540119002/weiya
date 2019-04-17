233b7f54be88a439a1cfb0a530d34eac166eabe1
zhangmin <540119002@qq.com>
2019-04-17 14:33:29 +0800

    zm

--- thinkphp5.1/api/public/vue/cart.js
+++ thinkphp5.1/api/public/vue/cart.js
@@ -1,7 +1,12 @@
-graph.options = {
-    color:'blue',
-    thickness:'3px'
-};
-graph.draw();
-console.log(cube(3)); // 27
-console.log(foo);    // 4.555806215962888
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+Vue.component('blog-post', {
+    props: ['title'],
+    template: '<h3>{{ title }}</h3>'
+});
