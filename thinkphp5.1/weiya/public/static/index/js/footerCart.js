ce010451e655ba6e574e2212b6af7092629e1f7c
zhangmin <540119002@qq.com>
2019-04-18 17:52:38 +0800

    zm

--- thinkphp5.1/weiya/public/static/index/js/footerCart.js
+++ thinkphp5.1/weiya/public/static/index/js/footerCart.js
@@ -11,7 +11,7 @@ function addCartCallBack(){
         url: url,
         data: postData,
         type: 'post',
-        dataType:'json',
+        processData:false,
         beforeSend: function(){
             $('.loading').show();
         },
