e4d7cedb80d6e108945b92ff32fac9ad832d50a5
zhangmin <540119002@qq.com>
2019-04-18 14:50:52 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Cart.php
+++ thinkphp5.1/weiya/application/index/controller/Cart.php
@@ -19,6 +19,7 @@ class Cart extends \common\controller\UserBase{
      *
      */
     public function addCart(){
+        return successMsg('成功');
         if(!request()->isPost()){
             return errorMsg('请求方式错误');
         }
