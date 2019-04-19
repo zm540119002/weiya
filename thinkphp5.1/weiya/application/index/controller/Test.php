4e4f9980cee4c8d0cb81de76427861c5365d2ebe
zhangmin <540119002@qq.com>
2019-04-19 15:32:47 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Test.php
+++ thinkphp5.1/weiya/application/index/controller/Test.php
@@ -3,13 +3,13 @@ namespace app\index\controller;
 class Test extends \common\controller\Base{
     /**首页
      */
-    public function index(){
+    public function test(){
         $unlockingFooterCart = unlockingFooterCartConfig([0,2,1]);
         $this->assign('unlockingFooterCart', $unlockingFooterCart);
         return $this->fetch();
     }
 
-    public function test(){
+    public function test1(){
         return $this->fetch();
     }
 
