f1101f744d4875a91046f2929d4a6146a164ce43
zm <zm@example.com>
2019-04-22 13:53:57 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Test.php
+++ thinkphp5.1/weiya/application/index/controller/Test.php
@@ -1,21 +1,28 @@
 <?php
 namespace app\index\controller;
 class Test extends \common\controller\Base{
-    /**首页
+    /**测试
      */
     public function test(){
         if(request()->isAjax()){
             $unlockingFooterCart = unlockingFooterCartConfig([0,2,1]);
-            return json($unlockingFooterCart);
+            return json_encode($unlockingFooterCart);
         }else{
+            $unlockingFooterCart = json_encode(unlockingFooterCartConfig([0,2,1]));
+            $this->assign('unlockingFooterCart',$unlockingFooterCart);
+
             return $this->fetch();
         }
     }
 
+    /**测试1
+     */
     public function test1(){
         return $this->fetch();
     }
 
+    /**测试2
+     */
     public function test2(){
         return $this->fetch();
     }
