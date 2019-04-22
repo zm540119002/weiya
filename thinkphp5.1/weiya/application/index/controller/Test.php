61563cfd8e588d59eadfce051db25ee4739e408d
zhangmin <540119002@qq.com>
2019-04-22 11:04:34 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Test.php
+++ thinkphp5.1/weiya/application/index/controller/Test.php
@@ -10,10 +10,10 @@ class Test extends \common\controller\Base{
         }else{
             $unlockingFooterCart = unlockingFooterCartConfig([0,2,1]);
             $unlockingFooterCart =json_encode($unlockingFooterCart,JSON_UNESCAPED_UNICODE);
-//            $this->assign('unlockingFooterCart',$unlockingFooterCart);
-            $this->assign([
-                'unlockingFooterCart'=>$unlockingFooterCart,
-            ]);
+            $this->assign('unlockingFooterCart',$unlockingFooterCart);
+//            $this->assign([
+//                'unlockingFooterCart'=>$unlockingFooterCart,
+//            ]);
             return $this->fetch();
         }
     }
