86ef1673aaec2e5b8352f003af3879e25a662923
zhangmin <540119002@qq.com>
2019-04-24 14:06:35 +0800

    zm

--- thinkphp5.1/weiya/application/common.php
+++ thinkphp5.1/weiya/application/common.php
@@ -62,7 +62,6 @@ function getUnit($num){
 function unlockingFooterCartConfig($arr){
     $footerCartConfig = config('footer_menu.menu');
     $tempArr = array();
-    $tempArr['count'] = count($arr);
     foreach ($arr as $val) {
         $tempArr['menu'][] = $footerCartConfig[$val];
     }
--- thinkphp5.1/weiya/application/index/config/footer_menu.php
+++ thinkphp5.1/weiya/application/index/config/footer_menu.php
@@ -16,132 +16,115 @@
 return [
     //底部购物车配置
     'menu' => [
-        [
+        [//0
             'name'   => '总金额',
             'class'=>[
                 'bottom_item',
                 'amount',
             ],
-        ],//0
-        [
+        ],[//1
             'name'   => '加入购物车',
             'class'=>[
                 'bottom_item',
                 'add_cart',
             ],
             'action'=>url('Cart/addCart'),
-        ],//1
-        [
+        ],[//2
             'name'   => '购物车',
             'class'=>[
                 'bottom_item',
                 'add_cart_icon',
             ],
-        ],//2
-        [
+        ],[//3
             'name'   => '立即购买',
             'class'=>[
                 'bottom_item',
                 'buy_now',
             ],
-        ],//3
-        [
+        ],[//4
             'name'   => '提交订单',
             'class'=>[
                 'bottom_item',
                 'confirm_order',
             ],
-        ],//4
-        [
+        ],[//5
             'name'   => '支付',
             'class'=>[
                 'bottom_item',
                 'pay',
             ],
-        ],//5
-        [
+        ],[//6
             'name'   => '增加地址',
             'class'=>[
                 'bottom_item',
                 'address_save',
             ],
-        ],//6
-        [
+        ],[//7
             'name'   => '修改地址',
             'class'=>[
                 'bottom_item',
                 'address_save',
             ],
-        ],//7
-        [
+        ],[//8
             'name'   => '新建地址',
             'class'=>[
                 'bottom_item',
                 'address_create',
             ],
-        ],//8
-        [
+        ],[//9
             'name'   => '去结算',
             'class'=>[
                 'bottom_item',
                 'settlement',
             ],
-        ],//9
-        [
+        ],[//10
             'name'   => '全选',
             'class'=>[
                 'bottom_item',
                 'checked_all',
             ],
-        ],//10
-        [
+        ],[//11
             'name'   => '提交订单',
             'class'=>[
                 'bottom_item',
                 'confirm_order',
             ],
-        ],//11
-        [
+        ],[//12
             'name'   => '确认收货',
             'class'=>[
                 'bottom_item',
                 'confirm_receive',
             ],
-        ],//12
-        [
+        ], [//13
             'name'   => '去评价',
             'class'=>[
                 'bottom_item',
                 'to_evaluate',
             ],
-        ],//13
-        [
+        ],[//14
             'name'   => '再次购买',
             'class'=>[
                 'bottom_item',
                 'purchase_again',
             ],
-        ],//14
-        [
+        ],[//15
             'name'   => '增加商标',
             'class'=>[
                 'bottom_item',
                 'add_brand',
             ],
-        ],//15
-        [
+        ],[//16
             'name'   => '立即提交',
             'class'=>[
                 'bottom_item',
                 'submit',
             ],
-        ],//16
-        [
+        ],[//17
             'name'   => '删除',
             'class'=>[
                 'bottom_item',
                 'delete',
             ],
-        ]//17
+        ]
     ],
 ];
