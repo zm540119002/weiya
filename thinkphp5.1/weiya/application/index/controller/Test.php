8a510e8dc960ea349efd68f06be12e45135c8145
zhangmin <540119002@qq.com>
2019-04-26 17:16:10 +0800

    zm

--- thinkphp5.1/weiya/application/index/controller/Test.php
+++ thinkphp5.1/weiya/application/index/controller/Test.php
@@ -65,9 +65,8 @@ class Test extends \common\controller\Base{
                 }
             }
             $productSelectParam = [
-                'buy_named' => '啊啊',
                 'step_quantity' => 10,
-//                'init_quantity' => 2,
+                'init_quantity' => 2,
             ];
             $this->assign('productSelectParam',json_encode($productSelectParam));
             return $this->fetch();
