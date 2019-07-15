b1ee882e9326f759e6b70743117e3af9b7610bab
yangguanbao <yangguanbao@qq.com>
2019-07-15 10:23:33 +0800

    'ygb'

--- thinkphp5.1/api/application/ucenter/controller/UserCenterApi.php
+++ thinkphp5.1/api/application/ucenter/controller/UserCenterApi.php
@@ -66,7 +66,7 @@ class UserCenterAPi extends \common\controller\BaseApi{
     public function test(){
         $token = input('post.token');
         cache('aa',12);
-        print_r(123);
+        return 123;
         print_r(cache('aa'));
     }
 }
