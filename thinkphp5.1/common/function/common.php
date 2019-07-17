34269ae7afab3ecb36c9c1e6b52e7d0a1cadd205
yangguanbao <yangguanbao@qq.com>
2019-07-17 15:01:07 +0800

    'ygb'

--- thinkphp5.1/common/function/common.php
+++ thinkphp5.1/common/function/common.php
@@ -978,55 +978,7 @@ function p($data){
 }
 
 
-function getToken($data = [],$expTime = 365*30*24*60*60)
-{
-    $key = "huang";  //这里是自定义的一个随机字串，应该写在config文件中的，解密时也会用，相当    于加密中常用的 盐  salt
-    $token = [
-        "iss" => "",  //签发者 可以为空
-        "aud" => "", //面象的用户，可以为空
-        "iat" => time(), //签发时间
-        "nbf" => time(), //在什么时候jwt开始生效  （这里表示生成100秒后才生效）
-        "exp" => time() + $expTime, //token 过期时间
-    ];
-    $token = array_merge($token,$data);
-    $jwt =  \common\component\jwt\JWT::encode($token, $key, "HS256"); //根据参数生成了 token
-    return $jwt;
-}
 
-
-function isCheck()
-{
-    $token = request()->header()['token'];
-    $key = "huang";  //上一个方法中的 $key 本应该配置在 config文件中的
-    try {
-        $jwtAuth = json_encode(\common\component\jwt\JWT::decode($token, $key, array('HS256')));
-        $authInfo = json_decode($jwtAuth, true);
-        if (!empty($authInfo['id'] && !empty($authInfo['mobile_phone']))) {
-            $msg = [
-                'status' => 1001,
-                'msg' => 'Token验证通过'
-            ];
-        } else {
-            $msg = [
-                'status' => 1002,
-                'msg' => 'Token验证不通过,用户不存在'
-            ];
-        }
-        return $msg;
-    } catch (\common\component\jwt\BeforeValidException $e) {
-        return [
-            'status' => 1003,
-            'msg' => 'Token无效'
-        ];
-    } catch (\common\component\jwt\ExpiredException $e) {
-        return [
-            'status' => 1004,
-            'msg' => 'Token过期'
-        ];
-    } catch (Exception $e) {
-        return $e;
-    }
-}
 
 
 function buildSuccess($data, $msg = '操作成功',$code) {
