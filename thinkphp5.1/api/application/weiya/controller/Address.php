6aa906f690a6c68cf2b2f1cb10b050613030b637
yangguanbao <yangguanbao@qq.com>
2019-07-17 16:23:51 +0800

    'ygb'

--- thinkphp5.1/api/application/weiya/controller/Address.php
+++ thinkphp5.1/api/application/weiya/controller/Address.php
@@ -106,10 +106,7 @@ class Address extends \common\controller\UserBaseApi{
             return errorMsg(config('custom.not_ajax'));
         }
         $model = new \common\model\Address();
-        $data = request()->get();
-        print_r($data);exit;
-        $id = (int)$data['data']['id'];
-        print_r($id);exit;
+        $id = input('get.id',0,'int');
         $config = [
             'where'=>[
                 ['status','=',0],
