07facf4110276d04211a234a619ebd42cc8aa7e4
yangguanbao <yangguanbao@qq.com>
2019-07-18 10:16:02 +0800

    'ygb'

--- thinkphp5.1/api/application/ucenter/controller/Address.php
+++ thinkphp5.1/api/application/ucenter/controller/Address.php
@@ -9,7 +9,6 @@ class Address extends \common\controller\UserBaseApi{
         $model = new \common\model\Address();
         $userId = $this->user['id'];
         $data = input('post.');
-        print_r($data);exit;
         $data = $data['data'];
         if($data['id'] ){
             //开启事务
