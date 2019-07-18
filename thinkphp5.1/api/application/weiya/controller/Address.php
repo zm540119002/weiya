7012d7fe56ee841356541096d1d1382d0df3fad3
yangguanbao <yangguanbao@qq.com>
2019-07-18 09:20:15 +0800

    'ygb'

--- thinkphp5.1/api/application/weiya/controller/Address.php
+++ thinkphp5.1/api/application/weiya/controller/Address.php
@@ -135,7 +135,7 @@ class Address extends \common\controller\UserBaseApi{
         ];
         $result = $model -> del($condition);
         if($result['status']){
-            return buildSuccess([],'删除成功');
+            return buildSuccess($data,'删除成功');
         }else{
             return buildFailed('删除失败');
         }
