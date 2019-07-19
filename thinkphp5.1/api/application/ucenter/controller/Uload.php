e41334ecb91e0e3c9b2feddcf7903e12f9c5c3ab
yangguanbao <yangguanbao@qq.com>
2019-07-19 10:30:08 +0800

    'ygb'

---
+++ thinkphp5.1/api/application/ucenter/controller/Uload.php
@@ -0,0 +1,57 @@
+<?php
+namespace app\weiya\controller;
+
+class Uload extends \common\controller\BaseApi{
+    //返回图片临时相对路径
+    public function uploadFileToTemp(){
+        $postData = input('post.');
+        $postData = $postData['data'];
+        $savePath = isset($_POST['uploadpath']) ? $_POST['uploadpath'] : config('upload_dir.temp_path');
+        if(is_string($postData['fileBase64'])){
+            if(strpos($postData['fileBase64'],'data:image') !==false || strpos($postData['fileBase64'],'data:video') !== false){
+                $result =  json_decode($this ->_uploadSingleFileToTemp($postData['fileBase64'],$savePath),true);
+                if(isset($result['code'])&& $result['code'] == -1){
+                    return $result['msg'];
+                }
+                return buildSuccess($result['data']);
+            }
+        }
+        if(is_array($postData['fileBase64'])){
+            $filesNew = [];
+            foreach ($postData['fileBase64'] as $k=>$file){
+                //判断是否为base64编码图片
+                if(strpos($file,'data:image') !==false || strpos($file,'data:video') !== false){
+                    $result =  $this ->_uploadSingleFileToTemp($file['fileSrc'],$savePath);
+                    if(isset($result['code'])&& $result['code'] == 0){
+                        return $result['msg'];
+                    }
+                    $filesNew[] = $result['data'][0];
+                }else{
+                    $filesNew[] = $file;
+                }
+            }
+            return buildSuccess($filesNew);
+        }
+    }
+    //返回图片临时相对路,上传多张图片带描述
+    public function uploadMultiFileToTempWithDes(){
+        $savePath = isset($_POST['uploadpath']) ? $_POST['uploadpath'] : config('upload_dir.temp_path');
+        $files = $_POST['imgsWithDes'];
+        $filesNew = [];
+        foreach ($files as $k=>$file){
+            //判断是否为base64编码图片
+            if(strpos($file['fileSrc'],'data:image') !==false || strpos($file['fileSrc'],'data:video') !== false){
+                $result =  $this ->_uploadSingleFileToTemp($file['fileSrc'],$savePath);
+                if(isset($result['code'])&& $result['code'] == 0){
+                    return $result['msg'];
+                }
+                $filesNew[$k]['fileSrc'] = $result['data'];
+                $filesNew[$k]['fileText'] = $file['fileText'];
+            }else{
+                $filesNew[$k] = $file;
+            }
+        }
+        return json_encode($filesNew);
+    }
+
+}
--- thinkphp5.1/common/controller/BaseApi.php
+++ thinkphp5.1/common/controller/BaseApi.php
@@ -26,57 +26,6 @@ class BaseApi extends \think\Controller{
         //去到页面跟返回跳转不一样，前端传参returnUrl
         session('returnUrl',input('get.returnUrl','')?:input('post.returnUrl',''));
     }
-    //返回图片临时相对路径
-    public function uploadFileToTemp(){
-        $postData = input('post.');
-        $postData = $postData['data'];
-        $savePath = isset($_POST['uploadpath']) ? $_POST['uploadpath'] : config('upload_dir.temp_path');
-        if(is_string($postData['fileBase64'])){
-            if(strpos($postData['fileBase64'],'data:image') !==false || strpos($postData['fileBase64'],'data:video') !== false){
-                $result =  json_decode($this ->uploadSingleFileToTemp($postData['fileBase64'],$savePath),true);
-                if(isset($result['code'])&& $result['code'] == -1){
-                    return $result['msg'];
-                }
-                return buildSuccess($result['data']);
-            }
-        }
-        if(is_array($postData['fileBase64'])){
-            $filesNew = [];
-            foreach ($postData['fileBase64'] as $k=>$file){
-                //判断是否为base64编码图片
-                if(strpos($file,'data:image') !==false || strpos($file,'data:video') !== false){
-                    $result =  $this ->uploadSingleFileToTemp($file['fileSrc'],$savePath);
-                    if(isset($result['code'])&& $result['code'] == 0){
-                        return $result['msg'];
-                    }
-                    $filesNew[] = $result['data'][0];
-                }else{
-                    $filesNew[] = $file;
-                }
-            }
-            return buildSuccess($filesNew);
-        }
-    }
-    //返回图片临时相对路,上传多张图片带描述
-    public function uploadMultiFileToTempWithDes(){
-        $savePath = isset($_POST['uploadpath']) ? $_POST['uploadpath'] : config('upload_dir.temp_path');
-        $files = $_POST['imgsWithDes'];
-        $filesNew = [];
-        foreach ($files as $k=>$file){
-            //判断是否为base64编码图片
-            if(strpos($file['fileSrc'],'data:image') !==false || strpos($file['fileSrc'],'data:video') !== false){
-                $result =  $this ->uploadSingleFileToTemp($file['fileSrc'],$savePath);
-                if(isset($result['code'])&& $result['code'] == 0){
-                    return $result['msg'];
-                }
-                $filesNew[$k]['fileSrc'] = $result['data'];
-                $filesNew[$k]['fileText'] = $file['fileText'];
-            }else{
-                $filesNew[$k] = $file;
-            }
-        }
-        return json_encode($filesNew);
-    }
 
     //上传单个data64位文件
     /**
@@ -84,7 +33,7 @@ class BaseApi extends \think\Controller{
      * @param $savePath 保存路径
      * @return array|string
      */
-    public function uploadSingleFileToTemp($fileBase64,$savePath){
+    public function _uploadSingleFileToTemp($fileBase64,$savePath){
         // 获取图片
         list($type, $data) = explode(',', $fileBase64);
         // 判断文件类型
