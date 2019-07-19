d57bfb5e549fef84c4b771796b2193261dea19ec
yangguanbao <yangguanbao@qq.com>
2019-07-19 11:02:16 +0800

    'ygb'

--- thinkphp5.1/api/application/ucenter/controller/Upload.php
+++ thinkphp5.1/api/application/ucenter/controller/Upload.php
@@ -27,12 +27,14 @@ class Upload extends \common\controller\BaseApi{
     //返回图片临时相对路,上传多张图片带描述
     public function uploadMultiFileToTempWithDes(){
         $savePath = isset($_POST['uploadpath']) ? $_POST['uploadpath'] : config('upload_dir.temp_path');
-        $files = $_POST['imgsWithDes'];
+        $postData = input('post.');
+        $postData = $postData['data'];
+        print_r($postData);exit;
         $filesNew = [];
         foreach ($files as $k=>$file){
             //判断是否为base64编码图片
             if(strpos($file['fileSrc'],'data:image') !==false || strpos($file['fileSrc'],'data:video') !== false){
-                $result =  $this ->_uploadSingleFileToTemp($file['fileSrc'],$savePath);
+                $result =  json_decode($this ->_uploadSingleFileToTemp($file['fileSrc'],$savePath),true);
                 if(isset($result['code'])&& $result['code'] == 0){
                     return $result['msg'];
                 }
