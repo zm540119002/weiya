92ab4013be7723dc7f2310c1dd4d67649ce7543e
yangguanbao <yangguanbao@qq.com>
2019-07-19 14:02:25 +0800

    'ygb'

--- thinkphp5.1/api/application/ucenter/controller/UserCenterApi.php
+++ thinkphp5.1/api/application/ucenter/controller/UserCenterApi.php
@@ -61,4 +61,48 @@ class UserCenterAPi extends \common\controller\BaseApi{
         return buildSuccess($response->Message);
     }
 
+
+
+    //修改头像
+    public function editAvatar(){
+        if(!request()->isPost()){
+            return buildFailed(config('custom.not_post'));
+        }
+        $user = isLogin();
+        print_r($user);exit;
+        $oldAvatar = $user['avatar'];
+        $fileBase64 = input('post.fileBase64');
+        $upload = config('upload_dir.user_avatar');
+        $newAvatar = uploadSingleFileToTemp($fileBase64,$upload);
+        if($newAvatar['status'] == 0 && !$newAvatar){
+            return $this->errorMsg('失败');
+        }
+        $user['avatar'] = $newAvatar;
+        $modelUser = new \common\model\User();
+        $result = $modelUser->allowField(['avatar'])->save($user, ['id' => $user['id']]);
+        if(!$result){
+            return $this->errorMsg('失败');
+        }
+        //删除旧详情图
+        delImgFromPaths($oldAvatar,$newAvatar);
+        setSession($user);
+        return successMsg('成功',['avatar'=>$newAvatar]);
+    }
+
+    //修改名字
+    public function editName(){
+        if(!request()->isPost()){
+            return $this->errorMsg('请求方式错误');
+        }
+        $modelUser = new \common\model\User();
+        $user = session('user');
+        $newName = preg_replace('# #','',input('post.name'));
+        $user['name'] = $newName;
+        $result = $modelUser->allowField(['name'])->save($user, ['id' => $user['id']]);
+        if(!$result){
+            return $this->errorMsg('失败');
+        }
+        setSession($user);
+        return successMsg('成功',['name'=>$newName]);
+    }
 }
