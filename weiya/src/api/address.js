e914f56cb0a8e8d27bb585534b5f80e28df7e791
Yuan Nan <554606686@qq.com>
2019-07-19 16:58:38 +0800

    nn

--- weiya/src/api/address.js
+++ weiya/src/api/address.js
@@ -5,6 +5,7 @@ export function addressHandle () {
   return request({
     url: '/ucenter/Address/getList',
     method: 'get',
+    headers: { 'Content-Type': 'application/json;charset=utf8', 'organId': '1333333333' },
     data: { }
   })
 }
