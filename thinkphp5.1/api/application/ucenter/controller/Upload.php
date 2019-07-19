c79c6239f16b02a7611d377c81998e6fb0e67abf
yangguanbao <yangguanbao@qq.com>
2019-07-19 10:39:33 +0800

    'ygb'

--- thinkphp5.1/api/application/ucenter/controller/Upload.php
+++ thinkphp5.1/api/application/ucenter/controller/Upload.php
@@ -17,9 +17,9 @@ class Upload extends \common\controller\BaseApi{
             }
         }
         if(is_array($postData['fileBase64'])){
-            print_r($postData['fileBase64']);exit;
             $filesNew = [];
             foreach ($postData['fileBase64'] as $k=>$file){
+                print_r($file);exit;
                 //判断是否为base64编码图片
                 if(strpos($file,'data:image') !==false || strpos($file,'data:video') !== false){
                     $result =  $this ->_uploadSingleFileToTemp($file['fileSrc'],$savePath);
